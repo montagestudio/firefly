dba86a8d95107a4c21e0540afe5bb5fedd8c0e4d
Mike Czepiel <mike@czepiel.net>
2013-11-13 17:34:19 -0800

    Make the environmentBridge the actual bridge
    The base prototype isn't really doing anything for us. Though we should
    eventually have one for documentation purposes.

--- inject/adaptor/client/core/environment-bridge.js
+++ inject/adaptor/client/core/environment-bridge.js
@@ -1,4 +1,5 @@
-var Montage = require("montage").Montage;
+var Montage = require("montage").Montage,
+    Promise = require("montage/core/promise").Promise;
 
 exports.EnvironmentBridge = Montage.specialize({
 
@@ -8,8 +9,56 @@ exports.EnvironmentBridge = Montage.specialize({
         }
     },
 
-    save: {
-        value: Function.noop
+    init: {
+        value: function (backendName) {
+            this._backendName = backendName;
+
+            return this;
+        }
+    },
+
+    projectInfo: {
+        get: function () {
+            return Promise.resolve({"fileUrl": require.getPackage({name: "palette"}).location + "templates/component.reel"});
+        }
+    },
+
+    setDocumentDirtyState: {
+        value: function () {
+        }
+    },
+
+    availableExtensions: {
+        get: function () {
+            return Promise.resolve(null);
+        }
+    },
+
+    promptForSave: {
+        value: function () {
+            return Promise.resolve(null);
+        }
+    },
+
+    openHttpUrl: {
+        value: function (url) {
+            var deferredWindow = Promise.defer(),
+                newWindow = window.open(url);
+
+            if (newWindow) {
+                deferredWindow.resolve(newWindow);
+            } else {
+                deferredWindow.reject( new Error("Failed to open window to " + url));
+            }
+
+            return deferredWindow.promise;
+        }
+    },
+
+    createComponent: {
+        value: function () {
+
+        }
     }
 
 });
