ef91779e5544004ad07838d430f5c7e05a805d7e
Stuart Knightley <stuart@stuartk.com>
2013-11-13 20:22:26 -0800

    Output correct port on server start

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function main(options) {
         .listen(options.port);
     })
     .then(function (server) {
-        console.log("Listening on http://127.0.0.1:8080");
+        console.log("Listening on http://127.0.0.1:" + options.port);
         return server;
     });
 }
