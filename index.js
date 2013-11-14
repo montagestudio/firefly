3fc07a95700e2bc651b4ad4072cf009c2a796bfc
Stuart Knightley <stuart@stuartk.com>
2013-11-13 16:28:53 -0800

    Structure for easier testing

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
 var joey = require("joey");
 var fs = require("q-io/fs");
 
+var commandOptions = {
+    "client": {
+        alias: "c",
+        describe: "A directory containing filament"
+    },
+    "port": {
+        alias: "p",
+        describe: "The port to run the server on",
+        default: 8080
+    }
+};
+
 var argv = require("optimist")
     .usage("Usage: $0 --client=<directory> [--port=<port>]")
     .demand(["client"])
-    .options({
-        "client": {
-            alias: "c",
-            describe: "A directory containing filament"
-        },
-        "port": {
-            alias: "p",
-            describe: "The port to run the server on",
-            default: 8080
+    .options(commandOptions).argv;
+
+module.exports = main;
+function main(options) {
+    options = options || {};
+    options.fs = options.fs || fs;
+
+    options.port = options.port || commandOptions.port.default;
+
+    return fs.exists(options.client)
+    .then(function (clientExists) {
+        if (!clientExists) {
+            throw new Error("Client directory '" + options.client + "' does not exist");
         }
-    }).argv;
 
-fs.exists(argv.client)
-.then(function (clientExists) {
-    if (!clientExists) {
-        throw new Error("Client directory '" + argv.client + "' does not exist");
-    }
+        return joey
+            .log()
+            .error()
+            .fileTree(options.client)
+            .listen(options.port);
+    })
+    .then(function () {
+        console.log("Listening on http://127.0.0.1:8080");
 
-    return joey
-        .log()
-        .error()
-        .fileTree(argv.client)
-        .listen(argv.port);
-})
-.then(function () {
-    console.log("Listening on http://127.0.0.1:8080");
+    });
+}
 
-})
-.done();
+if (require.main === module) {
+    main(argv).done();
+}
