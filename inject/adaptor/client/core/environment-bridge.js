46632bc006bdc014e87edb2e08b740a5b78f69f7
Mike Czepiel <mike@czepiel.net>
2013-11-13 22:08:11 -0800

    Fake enough of the services to get an "app" loaded

--- inject/adaptor/client/core/environment-bridge.js
+++ inject/adaptor/client/core/environment-bridge.js
@@ -1,6 +1,9 @@
 /*jshint browser:true */
 var Montage = require("montage").Montage,
     Promise = require("montage/core/promise").Promise;
+    FileDescriptor = require("./file-descriptor").FileDescriptor;
+
+var PROJECT_PROTOCOL = "fs:";
 
 exports.EnvironmentBridge = Montage.specialize({
 
@@ -18,9 +21,93 @@ exports.EnvironmentBridge = Montage.specialize({
         }
     },
 
-    projectInfo: {
+    _userName: {
+        value: null
+    },
+
+    _projectName: {
+        value: null
+    },
+
+    projectUrl: {
         get: function () {
-            return Promise.resolve({"fileUrl": require.getPackage({name: "palette"}).location + "templates/component.reel"});
+            var pathComponents = window.location.pathname.replace(/^\//, "").split("/"),
+                user,
+                project,
+                url = null;
+
+            if (pathComponents.length >= 2) {
+                this._userName = user = pathComponents[0];
+                this._projectName = project = pathComponents[1];
+                url = PROJECT_PROTOCOL + "//github/" + user + "/" + project;
+            }
+
+            return url;
+        }
+    },
+
+    projectInfo: {
+        value: function () {
+
+            //TODO use a final url when it's ready
+            // var packageUrl = window.location.origin + "/" + this._userName + "/" + this._projectName + "/tree";
+            var packageUrl = window.location.origin + "/clone";
+            var dependencies = [];
+
+            return Promise.resolve({
+                "fileUrl": this.projectUrl,
+                "packageUrl": packageUrl,
+                "dependencies": dependencies
+            });
+        }
+    },
+
+    previewUrl: {
+        get: function () {
+            //TODO replace with a url when available
+            return window.location.origin + "/clone/index.html";
+        }
+    },
+
+    list: {
+        value: function (url) {
+
+            //TODO actually implement this
+
+            var fakefd = FileDescriptor.create().initWithUrlAndStat("fs://localhost/foo.reel/", 0);
+
+            return Promise.resolve([fakefd]);
+        }
+    },
+
+    watch: {
+        value: function () {
+            console.log("watch", arguments)
+            return Promise.resolve(true);
+        }
+    },
+
+    getExtensionsAt: {
+        value: function () {
+            return [];
+        }
+    },
+
+    componentsInPackage: {
+        value: function () {
+            return Promise.resolve(null);
+        }
+    },
+
+    registerPreview: {
+        value: function () {
+            return Promise.resolve("");
+        }
+    },
+
+    launchPreview: {
+        value: function () {
+            return Promise.resolve(true);
         }
     },
 
