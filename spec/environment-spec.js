6d7347d12257b726d7f56accbc80b5462fb373e4
Stuart Knightley <stuart@stuartk.com>
2013-11-16 02:38:23 -0800

    Fix tests for xip.io urls

--- spec/environment-spec.js
+++ spec/environment-spec.js
@@ -3,7 +3,9 @@ var environment = require("../environment");
 describe("environment", function () {
     describe("getProjectUrl", function () {
         it("returns a url", function () {
-            expect(environment.getProjectUrl()).toEqual("http://localhost:2441");
+            expect(environment.getProjectUrl("owner/repo")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2441");
+            expect(environment.getProjectUrl("/owner/repo")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2441");
+            expect(environment.getProjectUrl("/owner/repo/fail")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2441");
         });
     });
 });
