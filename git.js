3a2587b91c85a7ac00ed9ccbc2bc2bf79178ae1d
Stuart Knightley <stuart@stuartk.com>
2013-11-15 20:37:38 -0800

    Add basic auth to git clone

--- git.js
+++ git.js
@@ -1,5 +1,6 @@
 var Q = require("q");
 var fs = require("q-io/fs");
+var URL = require("url");
 var exec = require("./exec");
 
 module.exports = Git;
@@ -15,6 +16,9 @@ Git.prototype.clone = function(cloneUrl, path) {
     if (!/^https:\/\//.test(cloneUrl)) {
         return Q.reject(new Error("Clone url must be https://, not " + cloneUrl));
     }
+    var parsed = URL.parse(cloneUrl);
+    parsed.auth = this._accessToken + ":" + "x-oauth-basic";
+    cloneUrl = URL.format(parsed);
     return exec("git", ["clone", cloneUrl, path], "/");
 };
 
