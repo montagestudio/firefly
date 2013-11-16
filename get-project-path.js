9cf432dc5e6892abbb0359f63fe7b3c350a8d145
Stuart Knightley <stuart@stuartk.com>
2013-11-16 13:33:59 -0800

    Update getProjectPath to use given session and pathname

--- get-project-path.js
+++ get-project-path.js
@@ -2,5 +2,7 @@ var FS = require("q-io/fs");
 
 module.exports = getProjectPath;
 function getProjectPath(session, pathname) {
-    return FS.join(process.cwd(), "..", "clone");
+    // FIXME, use same logic from environment.getProjectUrl
+    var match = pathname.match(/\/?([^\/]+)\/([^\/]+)/);
+    return FS.join(process.cwd(), "..", "clone", session.username, match[1], match[2]);
 }
