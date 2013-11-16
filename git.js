b144a1070cc95a0226d72fac32841cbe12757374
Stuart Knightley <stuart@stuartk.com>
2013-11-15 21:07:52 -0800

    Add git addRemote, add, commit
    and specs

--- git.js
+++ git.js
@@ -11,17 +11,31 @@ function Git(fs, accessToken) {
     this._accessToken = accessToken;
 }
 
-Git.prototype.init = function(path) {
-    return exec("git", ["init", path], "/");
+Git.prototype.init = function(repoPath) {
+    return exec("git", ["init", repoPath], "/");
 };
 
-Git.prototype.push = function(path, branch) {
+Git.prototype.addRemote = function (repoPath, url) {
+    url = this._addAccessToken(url);
+    return exec("git", ["remote", "add", "origin", url], repoPath);
+};
+
+Git.prototype.add = function(repoPath, paths) {
+    var args = ["add"].concat(paths);
+    return exec("git", args, repoPath);
+};
+
+Git.prototype.commit = function (repoPath, message) {
+    return exec("git", ["commit", "-m", message], repoPath);
+};
+
+Git.prototype.push = function(repoPath, branch) {
     var args = ["push", "origin"];
     if (typeof branch === "string") {
         args.push(branch);
     }
     // The remote has already been set with the accessToken in Git#clone
-    return exec("git", args, path);
+    return exec("git", args, repoPath);
 };
 
 // FIXME to avoid writing the accessToken to disk in .git/config do
@@ -29,18 +43,22 @@ Git.prototype.push = function(path, branch) {
 // cd foo
 // git init
 // git pull https://<token>@github.com/username/bar.git
-Git.prototype.clone = function(cloneUrl, path) {
+Git.prototype.clone = function(cloneUrl, repoPath) {
     if (!/^https:\/\//.test(cloneUrl)) {
         return Q.reject(new Error("Clone url must be https://, not " + cloneUrl));
     }
-    var parsed = URL.parse(cloneUrl);
-    parsed.auth = this._accessToken + ":" + "x-oauth-basic";
-    cloneUrl = URL.format(parsed);
-    return exec("git", ["clone", cloneUrl, path], "/");
+    cloneUrl = this._addAccessToken(cloneUrl);
+    return exec("git", ["clone", cloneUrl, repoPath], "/");
 };
 
-Git.prototype.isCloned = function(path) {
-    var dotGitPath = fs.join(path, ".git");
+Git.prototype.isCloned = function(repoPath) {
+    var dotGitPath = fs.join(repoPath, ".git");
 
     return fs.isDirectory(dotGitPath);
+};
+
+Git.prototype._addAccessToken = function (url) {
+    var parsed = URL.parse(url);
+    parsed.auth = this._accessToken + ":" + "x-oauth-basic";
+    return URL.format(parsed);
 };
--- spec/git-spec.js
+++ spec/git-spec.js
@@ -25,6 +25,64 @@ describe("Git", function () {
         });
     });
 
+    describe("addRemote", function () {
+        it("works", function (done) {
+            git.init(tmpPath)
+            .then(function () {
+                return git.addRemote(tmpPath, "https://github.com/montagejs/mousse.git");
+            })
+            .then(function () {
+                return fs.read(fs.join(tmpPath, ".git", "config"));
+            })
+            .then(function (config) {
+                expect(config.indexOf('[remote "origin"]')).not.toBe(-1);
+                expect(config.indexOf("url = https://xxx:x-oauth-basic@github.com/montagejs/mousse.git")).not.toBe(-1);
+            })
+            .then(done, done);
+        });
+    });
+
+    describe("add", function () {
+        it("works", function (done) {
+            git.init(tmpPath)
+            .then(function () {
+                fs.write(fs.join(tmpPath, "test.txt"), "pass");
+            })
+            .then(function () {
+                return git.add(tmpPath, ".");
+            })
+            .then(function () {
+                return fs.exists(fs.join(tmpPath, ".git", "index"));
+            })
+            .then(function (indexExists) {
+                expect(indexExists).toBe(true);
+            })
+            .then(done, done);
+        });
+    });
+
+    describe("commit", function () {
+        it("works", function (done) {
+            git.init(tmpPath)
+            .then(function () {
+                fs.write(fs.join(tmpPath, "test.txt"), "pass");
+            })
+            .then(function () {
+                return git.add(tmpPath, ".");
+            })
+            .then(function () {
+                return git.commit(tmpPath, "testing");
+            })
+            .then(function () {
+                return fs.exists(fs.join(tmpPath, ".git", "refs", "heads", "master"));
+            })
+            .then(function (masterExists) {
+                expect(masterExists).toBe(true);
+            })
+            .then(done, done);
+        });
+    });
+
     describe("clone", function () {
         it("works", function (done) {
             git.clone("https://github.com/montagejs/mousse.git", tmpPath)
