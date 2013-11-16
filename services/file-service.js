b6596d6bdf2ff3f89fc5c7e39680d0ed850f9c16
Stuart Knightley <stuart@stuartk.com>
2013-11-15 16:16:27 -0800

    Create file-service with given fs
    and add tests

--- services/file-service.js
+++ services/file-service.js
@@ -1,26 +1,8 @@
-var Q = require("q"),
-    QFS = require("q-io/fs"),
-    minimatch = require("minimatch"),
-    PATH = require('path'),
-    URL = require("url");
-
-var convertProjectUrlToPath = exports.convertProjectUrlToPath = function (url) {
-    var projectRootPath = PATH.join(process.cwd(), "..", "clone");
-    var extraPath = URL.parse(url).pathname;
-
-    return PATH.join(projectRootPath, extraPath);
-};
-
-var convertPathToProjectUrl = exports.convertPathToProjectUrl = function (path) {
-    var projectRootPath = PATH.join(process.cwd(), "..", "clone");
-    var projectHost = "http://localhost:2441";
-    return path.replace(projectRootPath, projectHost);
-};
-
-exports.read = function (url) {
-    var localPath = convertProjectUrlToPath(url);
-    return QFS.read(localPath);
-};
+var log = require("logging").from(__filename);
+var Q = require("q");
+var minimatch = require("minimatch");
+var PATH = require('path');
+var URL = require("url");
 
 var guard = function (exclude) {
     exclude = exclude || [];
@@ -33,79 +15,103 @@ var guard = function (exclude) {
     };
 };
 
-/**
- * Lists all the files in the given path except node_modules and dotfiles.
- * @param  {string} path An absolute path to a directory.
- * @return {Promise.<Array.<string>>} A promise for an array of paths.
- */
-exports.listTree = function (path, extraExclude) {
-    var exclude = ["node_modules", ".*"];
-    if (extraExclude) {
-        if (!Array.isArray(extraExclude)) {
-            extraExclude = [extraExclude];
-        }
-        exclude.push.apply(exclude, extraExclude);
+module.exports = FileService;
+function FileService(fs) {
+    log("creating service");
+
+    // Returned service
+    var service = {};
+
+    var convertProjectUrlToPath = exports.convertProjectUrlToPath = function (url) {
+        return URL.parse(url).pathname;
+    };
+
+    var convertPathToProjectUrl = exports.convertPathToProjectUrl = function (path) {
+        var projectHost = "http://localhost:2441";
+        return projectHost + path;
+    };
+
+    /**
+     * Converts an array of (absolute) paths to an array of objects with `url`
+     * and `stat` properties.
+     * @param  {Array.<string>} paths Absolute paths.
+     * @return {Promise.<Array.<{url, stat}>>}
+     */
+    function pathsToUrlStatArray(paths) {
+        return Q.all(paths.map(function (path) {
+            return fs.stat(path).then(function (stat) {
+                // Directories in URLs must have a trailing slash
+                if (stat.isDirectory()) {
+                    path += "/";
+                }
+
+                return {url: convertPathToProjectUrl(path), stat: stat};
+            });
+        }));
     }
-    return QFS.listTree(path, guard(exclude)).then(pathsToUrlStatArray);
-};
 
-exports.list = function (url) {
-    var localPath = convertProjectUrlToPath(url);
+    service.read = function (url) {
+        var localPath = convertProjectUrlToPath(url);
+        return fs.read(localPath);
+    };
 
-    return QFS.list(localPath).then(function (filenames) {
-        var paths = filenames.filter(function (name) {
-            return !(/^\./).test(name);
-        }).map(function (filename) {
-            return PATH.join(localPath, filename);
-        });
-
-        return pathsToUrlStatArray(paths);
-    });
-};
-
-/**
- * Lists all the files in a package except node_modules, dotfiles and files
- * matching the globs listed in the package.json "exclude" property.
- * @param  {string} path An absolute path to the package directory to list.
- * @return {Promise.<Array.<string>>} A promise for an array of paths.
- */
-exports.listPackage = function (url) {
-    var exclude = ["node_modules", ".*"];
-
-    var path = convertProjectUrlToPath(url);
-
-    return QFS.read(PATH.join(path, "package.json")).then(function (contents) {
-        var pkg = JSON.parse(contents);
-        return guard(exclude.concat(pkg.exclude || []));
-    }, function (err) {
-        return guard(exclude);
-    }).then(function (guard) {
-        return QFS.listTree(path, guard).then(pathsToUrlStatArray);
-    });
-};
-
-exports.open = function (thing) {
-    var done = Q.defer();
-    // opener(thing, done.makeNodeResolver());
-    done.reject(new Error("TODO Implement me"));
-    return done.promise;
-};
-
-/**
- * Converts an array of (absolute) paths to an array of objects with `url`
- * and `stat` properties.
- * @param  {Array.<string>} paths Absolute paths.
- * @return {Promise.<Array.<{url, stat}>>}
- */
-function pathsToUrlStatArray(paths) {
-    return Q.all(paths.map(function (path) {
-        return QFS.stat(path).then(function (stat) {
-            // Directories in URLs must have a trailing slash
-            if (stat.isDirectory()) {
-                path += "/";
+    /**
+     * Lists all the files in the given path except node_modules and dotfiles.
+     * @param  {string} path An absolute path to a directory.
+     * @return {Promise.<Array.<string>>} A promise for an array of paths.
+     */
+    service.listTree = function (path, extraExclude) {
+        var exclude = ["node_modules", ".*"];
+        if (extraExclude) {
+            if (!Array.isArray(extraExclude)) {
+                extraExclude = [extraExclude];
             }
+            exclude.push.apply(exclude, extraExclude);
+        }
+        return fs.listTree(path, guard(exclude)).then(pathsToUrlStatArray);
+    };
 
-            return {url: convertPathToProjectUrl(path), stat: stat};
+    service.list = function (url) {
+        var localPath = convertProjectUrlToPath(url);
+
+        return fs.list(localPath).then(function (filenames) {
+            var paths = filenames.filter(function (name) {
+                return !(/^\./).test(name);
+            }).map(function (filename) {
+                return PATH.join(localPath, filename);
+            });
+
+            return pathsToUrlStatArray(paths);
         });
-    }));
+    };
+
+    /**
+     * Lists all the files in a package except node_modules, dotfiles and files
+     * matching the globs listed in the package.json "exclude" property.
+     * @param  {string} path An absolute path to the package directory to list.
+     * @return {Promise.<Array.<string>>} A promise for an array of paths.
+     */
+    service.listPackage = function (url) {
+        var exclude = ["node_modules", ".*"];
+
+        var path = convertProjectUrlToPath(url);
+
+        return fs.read(PATH.join(path, "package.json")).then(function (contents) {
+            var pkg = JSON.parse(contents);
+            return guard(exclude.concat(pkg.exclude || []));
+        }, function (err) {
+            return guard(exclude);
+        }).then(function (guard) {
+            return fs.listTree(path, guard).then(pathsToUrlStatArray);
+        });
+    };
+
+    service.open = function (thing) {
+        var done = Q.defer();
+        // opener(thing, done.makeNodeResolver());
+        done.reject(new Error("TODO Implement me"));
+        return done.promise;
+    };
+
+    return service;
 }
---
+++ spec/services/file-service-spec.js
@@ -0,0 +1,32 @@
+var FileService = require("../../services/file-service");
+var MockFs = require("q-io/fs-mock");
+
+describe("file-service", function () {
+    var fs, service;
+    beforeEach(function () {
+        fs = MockFs({
+            "core": {
+                "core.js": ""
+            },
+            "package.json": "{}"
+        });
+
+        service = FileService(fs);
+    });
+
+    describe("list", function () {
+        it("works", function (done) {
+            return service.list("/")
+            .then(function (list) {
+                expect(list.length).toEqual(2);
+
+                expect(list[0].url).toBe("http://localhost:2441/core/");
+                expect(list[0].stat).toBeDefined();
+                expect(list[1].url).toBe("http://localhost:2441/package.json");
+                expect(list[1].stat).toBeDefined();
+            })
+            .then(done, done);
+        });
+    });
+
+});
