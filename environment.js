5bf768a827bc9ea4e868ef3f5a368bc6e849a261
François Frisch <francoisfrisch@gmail.com>
2013-11-16 22:18:58 -0800

    Make production mode work with wildcard subdomains

--- environment.js
+++ environment.js
@@ -9,17 +9,14 @@ module.exports = env;
 
 if(production) {
     env.app = {
-        hostname: process.env.FIREFLY_APP_HOST || "localhost",
+        hostname: process.env.FIREFLY_APP_HOST || "app.127.0.0.1.xip.io",
         port: process.env.FIREFLY_APP_PORT || 2440,
         protocol: process.env.FIREFLY_APP_PROTOCOL || "http"
     };
     env.project = {
-        hostname: process.env.FIREFLY_PROJECT_HOST || "127.0.0.1",
+        hostname: process.env.FIREFLY_PROJECT_HOST || "*.project.127.0.0.1.xip.io",
         port: process.env.FIREFLY_PROJECT_PORT || 2440,
         protocol: process.env.FIREFLY_PROJECT_PROTOCOL || "http"
-    };
-    env.getProjectUrl = function (pathname) {
-        throw new Error("TODO");
     };
 } else {
     env.app = {
@@ -32,60 +29,60 @@ if(production) {
         port: 2441,
         protocol: "http"
     };
-    env.getDetailsFromAppUrl = function (url) {
-        var pathname = URL.parse(url).pathname;
-
-        var match = pathname.match(/\/?([^\/]+)\/([^\/]+)/);
-        var owner = match[1];
-        var repo = match[2];
-
-        return {
-            owner: owner,
-            repo: repo
-        };
-    };
-    env.getDetailsfromProjectUrl = function (url) {
-        // Needed because node's URL.parse interprets
-        // a.b.c.com:1234
-        // as
-        // protocol: "a.b.c.d.com"
-        // hostname: "2440"
-        if (!/^https?:\/\//.test(url)) {
-            url = "http://" + url;
-        }
-        var hostname = URL.parse(url).hostname;
-
-        var match = hostname.match(/([a-z\-]+)-([a-z]+)\./i);
-        var owner = match[1];
-        var repo = match[2];
-
-        return {
-            owner: owner,
-            repo: repo
-        };
-    };
-    env.getProjectUrlFromAppUrl = function (url) {
-        var details = this.getDetailsFromAppUrl(url);
-        var urlObj = Object.create(this.project);
-        urlObj.hostname = details.owner + "-" + details.repo + "." + urlObj.hostname;
-        return URL.format(urlObj);
-    };
-    env.getProjectPathFromSessionAndAppUrl = function (session, url) {
-        var details = this.getDetailsFromAppUrl(url);
-
-        // FIXME not to use FS
-        var FS = require("q-io/fs");
-        return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
-    };
-    env.getProjectPathFromSessionAndProjectUrl = function (session, url) {
-        var details = this.getDetailsfromProjectUrl(url);
-
-        // FIXME not to use FS
-        var FS = require("q-io/fs");
-        return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
-    };
 }
 
 env.getAppUrl = function () {
     return URL.format(this.app);
+};
+env.getDetailsFromAppUrl = function (url) {
+    var pathname = URL.parse(url).pathname;
+
+    var match = pathname.match(/\/?([^\/]+)\/([^\/]+)/);
+    var owner = match[1];
+    var repo = match[2];
+
+    return {
+        owner: owner,
+        repo: repo
+    };
+};
+env.getDetailsfromProjectUrl = function (url) {
+    // Needed because node's URL.parse interprets
+    // a.b.c.com:1234
+    // as
+    // protocol: "a.b.c.d.com"
+    // hostname: "2440"
+    if (!/^https?:\/\//.test(url)) {
+        url = "http://" + url;
+    }
+    var hostname = URL.parse(url).hostname;
+
+    var match = hostname.match(/([a-z\-]+)-([a-z]+)\./i);
+    var owner = match[1];
+    var repo = match[2];
+
+    return {
+        owner: owner,
+        repo: repo
+    };
+};
+env.getProjectUrlFromAppUrl = function (url) {
+    var details = this.getDetailsFromAppUrl(url);
+    var urlObj = Object.create(this.project);
+    urlObj.hostname = details.owner + "-" + details.repo + "." + urlObj.hostname;
+    return URL.format(urlObj);
+};
+env.getProjectPathFromSessionAndAppUrl = function (session, url) {
+    var details = this.getDetailsFromAppUrl(url);
+
+    // FIXME not to use FS
+    var FS = require("q-io/fs");
+    return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
+};
+env.getProjectPathFromSessionAndProjectUrl = function (session, url) {
+    var details = this.getDetailsfromProjectUrl(url);
+
+    // FIXME not to use FS
+    var FS = require("q-io/fs");
+    return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
 };
--- multiplex.js
+++ multiplex.js
@@ -1,6 +1,7 @@
 var Env = require("./environment");
 var Q = require("q");
 var joey = require("joey");
+var Status = require("q-io/http-apps/status");
 
 
 module.exports = multiplex;
@@ -12,11 +13,14 @@ function multiplex(options, appChainFactory, appChainOptions, projectChainFactor
                 projectChainFactory(projectChainOptions)
             ])
             .spread(function (appChain, projectChain) {
-                return joey.host(Env.app.hostname+":*", Env.project.hostname+":*")
-                    .hosts(function (branch) {
-                        branch(Env.app.hostname+":*").app(appChain.end());
-                        branch(Env.project.hostname+":*").app(projectChain).end();
-                    })
+
+                var contraint = Constrain(Host).call(joey, Env.app.hostname+":*", Env.project.hostname+":*");
+
+                var multiplex = Multiplex(Host).call(contraint, function (branch) {
+                    branch(Env.app.hostname+":*").app(appChain.end());
+                    branch(Env.project.hostname+":*").app(projectChain.end());
+                });
+                return multiplex
                     .listen(Env.app.port)
                     .then(function (server) {
                         return [
@@ -30,6 +34,26 @@ function multiplex(options, appChainFactory, appChainOptions, projectChainFactor
                             }
                         ];
                     });
+
+//                return joey
+//                    .host(Env.app.hostname+":*", Env.project.hostname+":*")
+//                    .hosts(function (branch) {
+//                        branch(Env.app.hostname+":*").app(appChain.end());
+//                        branch(Env.project.hostname+":*").app(projectChain).end();
+//                    })
+//                    .listen(Env.app.port)
+//                    .then(function (server) {
+//                        return [
+//                            {
+//                                chain: appChain,
+//                                server: server
+//                            },
+//                            {
+//                                chain: projectChain,
+//                                server: server
+//                            }
+//                        ];
+//                    });
             });
     } else {
         return Q.all([
@@ -58,3 +82,110 @@ function multiplex(options, appChainFactory, appChainOptions, projectChainFactor
     // TODO: multiplex based on request.headers.host, instead of starting
     // two servers on different ports
 }
+
+
+var Host = function (appForHost, notAcceptable) {
+    var table = Object.keys(appForHost).map(function (pattern) {
+        var parts = pattern.split(":");
+        var hostname = parts[0];
+        if(hostname) {
+            // store part in reverse order
+            var hostnameParts = hostname.split(".");
+            hostname = [];
+            var i = hostnameParts.length;
+            while(i--) {
+                hostname.push(hostnameParts[i]);
+            }
+        }
+        return [
+            pattern,
+            hostname || ["*"],
+            parts[1] || "*",
+            appForHost[pattern]
+        ];
+    });
+    if (!notAcceptable) {
+        notAcceptable = Status.notAcceptable;
+    }
+    return function (request, response) {
+        var matchHostname = function(hostname, requestHostname) {
+            var hostnameParts = requestHostname.split(":")[0].split(".");
+            var hostnameIndex = 0;
+            var i = hostnameParts.length;
+            var isMatch = true;
+            while(i--) {
+                 // check starting
+                var part = hostname[hostnameIndex];
+                if(part) {
+                    if ( part === "*") {
+                        break;
+                    } else if( part !== hostnameParts[i]) {
+                        isMatch = false;
+                        break;
+                    }
+                } else {
+                    isMatch = false;
+                    break;
+                }
+                hostnameIndex++;
+            }
+            return isMatch;
+        };
+        // find first matching host for app
+        for (var index = 0; index < table.length; index++) {
+            var row = table[index]; // [hostname, port, app]
+            var pattern = row[0];
+            var hostname = row[1];
+            var port = row[2];
+            var app = row[3];
+            if (
+                matchHostname(hostname, request.hostname) &&
+                (port === "*" || port === "" + request.port)
+            ) {
+                if (!request.terms) {
+                    request.terms = {};
+                }
+                request.terms.host = pattern;
+                return app(request, response);
+            }
+        }
+        return notAcceptable(request, response);
+    };
+};
+
+
+//Copied from joey
+
+var Multiplex = function (Using) {
+    return function (setup) {
+        var branches = {};
+        var chain = this;
+        setup(function () {
+            var branch = new chain.constructor();
+            Array.prototype.forEach.call(arguments, function (name) {
+                branches[name] = branch;
+            });
+            return branch;
+        });
+        return this.use(function (next) {
+            Object.keys(branches).forEach(function (name) {
+                var branch = branches[name];
+                branches[name] = branch.end();
+            });
+            return Using(branches, next);
+        });
+    };
+};
+
+var Constrain = function (Using) {
+    return function () {
+        var args = arguments;
+        var types = {};
+        return this.use(function (next) {
+            Array.prototype.forEach.call(args, function (type) {
+                types[type] = next;
+            });
+            return Using(types);
+        });
+    }
+};
