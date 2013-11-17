223fbeb4fa423a0842525c4b68f5e01c84fb3f6e
François Frisch <francoisfrisch@gmail.com>
2013-11-17 01:09:56 -0800

    Add new production wildcard domains scheme

--- environment.js
+++ environment.js
@@ -1,88 +1,180 @@
 var URL = require("url");
 
-var production = process.env.NODE_ENV === "production";
+function Env(options) {
+    var env = options || {  production: process.env.NODE_ENV === "production" };
+    env.getAppUrl = function () {
+        return URL.format(this.app);
+    };
 
-var env = {
-    production: !!production
-};
-module.exports = env;
+    //App works the same on dev and production
+    env.getDetailsFromAppUrl = function (url) {
+        var pathname = URL.parse(url).pathname;
 
-if(production) {
-    env.app = {
-        hostname: process.env.FIREFLY_APP_HOST || "app.127.0.0.1.xip.io",
-        port: process.env.FIREFLY_APP_PORT || 2440,
-        protocol: process.env.FIREFLY_APP_PROTOCOL || "http"
+        var match = pathname.match(/\/?([^\/]+)\/([^\/]+)/);
+        var owner = match[1];
+        var repo = match[2];
+
+        return {
+            owner: owner,
+            repo: repo
+        };
     };
-    env.project = {
-        hostname: process.env.FIREFLY_PROJECT_HOST || "*.project.127.0.0.1.xip.io",
-        port: process.env.FIREFLY_PROJECT_PORT || 2440,
-        protocol: process.env.FIREFLY_PROJECT_PROTOCOL || "http"
+    env.getProjectPathFromSessionAndAppUrl = function (session, url) {
+        var details = this.getDetailsFromAppUrl(url);
+
+        // FIXME not to use FS
+        var FS = require("q-io/fs");
+        return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
     };
-} else {
-    env.app = {
-        hostname: "app.127.0.0.1.xip.io",
-        port: 2440,
-        protocol: "http"
-    };
-    env.project = {
-        hostname: "project.127.0.0.1.xip.io",
-        port: 2441,
-        protocol: "http"
-    };
+
+    // PRODUCTION
+    if ( env.production) {
+        env.app = {
+            hostname: process.env.FIREFLY_APP_HOST || "app.127.0.0.1.xip.io",
+            port: process.env.FIREFLY_APP_PORT || 2440,
+            protocol: process.env.FIREFLY_APP_PROTOCOL || "http"
+        };
+        env.project = {
+            hostname: process.env.FIREFLY_PROJECT_HOST || "*.project.127.0.0.1.xip.io",
+            port: process.env.FIREFLY_PROJECT_PORT || 2440,
+            protocol: process.env.FIREFLY_PROJECT_PROTOCOL || "http"
+        };
+
+        var projectList = [];
+        var projectMemory = {};
+        var projectIndex = -1;
+        var projectInMemory = function addProject(project) {
+            // first check if we already have a number
+            return projectMemory[project.owner + "-" + project.repo];
+        };
+        var removeProjectInMemory = function addProject(project) {
+            delete projectMemory[project.owner + "-" + project.repo] ;
+        };
+        var addProjectInMemory = function addProject(project, number) {
+            projectMemory[project.owner + "-" + project.repo] = number;
+        };
+
+        var getProjectNumber = function addProject(project) {
+            // first check if we already have a number
+            var projectNumber = projectInMemory(project);
+
+            if (projectNumber != null) {
+                return projectNumber;
+            }
+
+            projectIndex++;
+            if(projectIndex > 40) {
+                projectIndex = 0;
+            }
+            // remove old one if one exists
+            removeProjectInMemory(project);
+            projectList[projectIndex] = project;
+            // add new one if one exists
+            addProjectInMemory(project, projectIndex);
+            return projectIndex;
+        };
+        env.getDetailsfromProjectUrl = function (url) {
+            // Needed because node's URL.parse interprets
+            // a.b.c.com:1234
+            // as
+            // protocol: "a.b.c.d.com"
+            // hostname: "2440"
+            if (!/^https?:\/\//.test(url)) {
+                url = "http://" + url;
+            }
+
+            var number = URL.parse(url).hostname.match(/p(\d+)\./i)[1];
+
+            var project = projectList[parseInt(number, 10)];
+
+            return {
+                owner: project.owner,
+                repo: project.repo
+            };
+        };
+        env.getProjectUrlFromAppUrl = function (url) {
+            var details = this.getDetailsFromAppUrl(url);
+            var urlObj = Object.create(this.project);
+            urlObj.hostname = "p" + getProjectNumber(details) + urlObj.hostname.substring(1, urlObj.hostname.length);
+            return URL.format(urlObj);
+        };
+        env.getProjectPathFromSessionAndProjectUrl = function (session, url) {
+            var details = this.getDetailsfromProjectUrl(url);
+
+            // FIXME not to use FS
+            var FS = require("q-io/fs");
+            return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
+        };
+
+    } else {
+
+        env.app = {
+            hostname: "app.127.0.0.1.xip.io",
+            port: 2440,
+            protocol: "http"
+        };
+        env.project = {
+            hostname: "project.127.0.0.1.xip.io",
+            port: 2441,
+            protocol: "http"
+        };
+        env.getDetailsFromAppUrl = function (url) {
+            var pathname = URL.parse(url).pathname;
+
+            var match = pathname.match(/\/?([^\/]+)\/([^\/]+)/);
+            var owner = match[1];
+            var repo = match[2];
+
+            return {
+                owner: owner,
+                repo: repo
+            };
+        };
+        env.getDetailsfromProjectUrl = function (url) {
+            // Needed because node's URL.parse interprets
+            // a.b.c.com:1234
+            // as
+            // protocol: "a.b.c.d.com"
+            // hostname: "2440"
+            if (!/^https?:\/\//.test(url)) {
+                url = "http://" + url;
+            }
+            var hostname = URL.parse(url).hostname;
+
+            var match = hostname.match(/([a-z\-]+)-([a-z]+)\./i);
+            var owner = match[1];
+            var repo = match[2];
+
+            return {
+                owner: owner,
+                repo: repo
+            };
+        };
+        env.getProjectUrlFromAppUrl = function (url) {
+            var details = this.getDetailsFromAppUrl(url);
+            var urlObj = Object.create(this.project);
+            urlObj.hostname = details.owner + "-" + details.repo + "." + urlObj.hostname;
+            return URL.format(urlObj);
+        };
+        env.getProjectPathFromSessionAndAppUrl = function (session, url) {
+            var details = this.getDetailsFromAppUrl(url);
+
+            // FIXME not to use FS
+            var FS = require("q-io/fs");
+            return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
+        };
+        env.getProjectPathFromSessionAndProjectUrl = function (session, url) {
+            var details = this.getDetailsfromProjectUrl(url);
+
+            // FIXME not to use FS
+            var FS = require("q-io/fs");
+            return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
+        };
+
+    }
+    return env;
 }
 
-env.getAppUrl = function () {
-    return URL.format(this.app);
-};
-env.getDetailsFromAppUrl = function (url) {
-    var pathname = URL.parse(url).pathname;
-
-    var match = pathname.match(/\/?([^\/]+)\/([^\/]+)/);
-    var owner = match[1];
-    var repo = match[2];
-
-    return {
-        owner: owner,
-        repo: repo
-    };
-};
-env.getDetailsfromProjectUrl = function (url) {
-    // Needed because node's URL.parse interprets
-    // a.b.c.com:1234
-    // as
-    // protocol: "a.b.c.d.com"
-    // hostname: "2440"
-    if (!/^https?:\/\//.test(url)) {
-        url = "http://" + url;
-    }
-    var hostname = URL.parse(url).hostname;
-
-    var match = hostname.match(/([a-z\-]+)-([a-z]+)\./i);
-    var owner = match[1];
-    var repo = match[2];
-
-    return {
-        owner: owner,
-        repo: repo
-    };
-};
-env.getProjectUrlFromAppUrl = function (url) {
-    var details = this.getDetailsFromAppUrl(url);
-    var urlObj = Object.create(this.project);
-    urlObj.hostname = details.owner + "-" + details.repo + "." + urlObj.hostname;
-    return URL.format(urlObj);
-};
-env.getProjectPathFromSessionAndAppUrl = function (session, url) {
-    var details = this.getDetailsFromAppUrl(url);
-
-    // FIXME not to use FS
-    var FS = require("q-io/fs");
-    return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
-};
-env.getProjectPathFromSessionAndProjectUrl = function (session, url) {
-    var details = this.getDetailsfromProjectUrl(url);
-
-    // FIXME not to use FS
-    var FS = require("q-io/fs");
-    return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
-};
+module.exports = Env();
+// for testing
+module.exports.Env = Env;
--- spec/environment-spec.js
+++ spec/environment-spec.js
@@ -1,11 +1,39 @@
-var environment = require("../environment");
+var Env = require("../environment").Env;
 
 describe("environment", function () {
-    describe("getProjectUrlFromAppUrl", function () {
-        it("returns a url", function () {
-            expect(environment.getProjectUrlFromAppUrl("owner/repo")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2441");
-            expect(environment.getProjectUrlFromAppUrl("/owner/repo")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2441");
-            expect(environment.getProjectUrlFromAppUrl("/owner/repo/fail")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2441");
+    var environment;
+    describe("in developement", function () {
+        beforeEach(function () {
+            environment = Env();
+        });
+        describe("getProjectUrlFromAppUrl", function () {
+            it("returns a url", function () {
+                expect(environment.getProjectUrlFromAppUrl("owner/repo")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2441");
+                expect(environment.getProjectUrlFromAppUrl("/owner/repo")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2441");
+                expect(environment.getProjectUrlFromAppUrl("/owner/repo/fail")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2441");
+            });
         });
     });
+    describe("in production", function () {
+        beforeEach(function () {
+            environment = Env({  production: true });
+        });
+        describe("getProjectUrlFromAppUrl", function () {
+            it("returns a url", function () {
+                expect(environment.getProjectUrlFromAppUrl("owner/repo")).toEqual("http://p0.project.127.0.0.1.xip.io:2440");
+                expect(environment.getProjectUrlFromAppUrl("/owner/repo2")).toEqual("http://p1.project.127.0.0.1.xip.io:2440");
+                expect(environment.getProjectUrlFromAppUrl("/owner/repo3/fail")).toEqual("http://p2.project.127.0.0.1.xip.io:2440");
+            });
+            it("can be recovered from the project domain", function () {
+                var detail = environment.getDetailsfromProjectUrl(environment.getProjectUrlFromAppUrl("owner/repo"));
+                expect(detail.owner).toEqual("owner");
+                expect(detail.repo).toEqual("repo");
+            });
+            it("should return a stable url", function () {
+                expect(environment.getProjectUrlFromAppUrl("owner/repo")).toEqual("http://p0.project.127.0.0.1.xip.io:2440");
+                expect(environment.getProjectUrlFromAppUrl("/owner/repo")).toEqual("http://p0.project.127.0.0.1.xip.io:2440");
+            });
+        });
+    });
+
 });
