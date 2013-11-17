cf8a3dc6574120119cb58e67e6a777c855dd199c
António Afonso <antonio.afonso@gmail.com>
2013-11-16 20:42:56 -0800

    Add RepositoryController
    For use with the bridge. In the future these operations should be made through the websocket.

---
+++ inject/adaptor/client/core/repository-controller.js
@@ -0,0 +1,85 @@
+var Montage = require("montage").Montage;
+var Promise = require("montage/core/promise").Promise;
+var getGithubApi = require("./github");
+
+exports.RepositoryController = Montage.specialize({
+    owner: {
+        value: null
+    },
+
+    repo: {
+        value: null
+    },
+
+    constructor: {
+        value: function RepositoryController() {
+
+        }
+    },
+
+    init: {
+        value: function(owner, repo) {
+            this.owner = owner;
+            this.repo = repo;
+
+            return this;
+        }
+    },
+
+    initializeRepositoryWorkspace: {
+        value: function() {
+            return this._request({
+                method: "POST",
+                url: "/" + this.owner + "/" + this.repo + "/init"
+            });
+        }
+    },
+
+    isRepositoryEmpty: {
+        value: function() {
+            var self = this;
+
+            return getGithubApi()
+            .then(function(githubApi) {
+                return githubApi.isRepositoryEmpty(self.owner, self.repo);
+            });
+        }
+    },
+
+    _request: {
+        value: function(request) {
+            var xhr = new XMLHttpRequest(),
+                deferred = Promise.defer();
+
+            xhr.open(request.method, request.url);
+            xhr.addEventListener("load", function() {
+                var message;
+
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    if (xhr.responseText) {
+                        try {
+                            message = JSON.parse(xhr.responseText);
+                        } catch (ex) {
+                            deferred.reject(ex.message);
+                        }
+                    }
+                    deferred.resolve(message);
+                } else {
+                    deferred.reject(xhr);
+                }
+            }, false);
+            xhr.addEventListener("error", function() {
+                deferred.reject(xhr);
+            }, false);
+
+            if (request.data) {
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.send(JSON.stringify(request.data));
+            } else {
+                xhr.send();
+            }
+
+            return deferred.promise;
+        }
+    }
+});
