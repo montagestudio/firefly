a8d5c69ce13ff3b08916ceea8dd4fec4a1ed8081
Mike Czepiel <mike@czepiel.net>
2013-11-17 20:56:06 -0800

    Add modalOverlay for presenting modal stuff

---
+++ inject/adaptor/client/ui/modal-overlay.reel/modal-overlay.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/modal-overlay.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ModalOverlay
+ * @extends Component
+ */
+exports.ModalOverlay = Component.specialize(/** @lends ModalOverlay# */ {
+    constructor: {
+        value: function ModalOverlay() {
+            this.super();
+        }
+    }
+});
