9440283ccf81886b274ce50b03846f490156cd76
António Afonso <antonio.afonso@gmail.com>
2013-11-17 19:05:54 -0800

    Add userController
    userController is a promise to the actual userController object.

---
+++ inject/adaptor/client/core/user-controller.js
@@ -0,0 +1,31 @@
+var Montage = require("montage").Montage;
+var github = require("./github");
+
+var UserController = Montage.specialize({
+    constructor: {
+        value: function UserController() {
+            this.super();
+        }
+    },
+
+    init: {
+        value: function() {
+            var self = this;
+
+            return github.githubApi()
+            .then(function(githubApi) {
+                return githubApi.getUser();
+            })
+            .then(function(user) {
+                self.name = user.name || user.login;
+                //jshint -W106
+                self.avatarUrl = user.avatar_url;
+                self.url = user.html_url;
+                //jshint +W106
+                return self;
+            });
+        }
+    }
+});
+
+exports.userController = new UserController().init();
