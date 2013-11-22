43c6716ad53036989c8bdf74ada774ffa6b59380
Stuart Knightley <stuart@stuartk.com>
2013-11-21 18:07:52 -0800

    Create project in lowercase
    Linux is case sensitive, but we lowercase the details we get from the
    app subdomain and url.

--- setup-project-workspace.js
+++ setup-project-workspace.js
@@ -8,8 +8,8 @@ function SetupProjectWorkspace(directory, minitPath) {
     return function(next) {
         return function(request, response) {
             var session = request.session,
-                owner = sanitize.sanitizeDirectoryName(request.params.owner),
-                repo = sanitize.sanitizeDirectoryName(request.params.repo),
+                owner = sanitize.sanitizeDirectoryName(request.params.owner).toLowerCase(),
+                repo = sanitize.sanitizeDirectoryName(request.params.repo).toLowerCase(),
                 workspacePath = fs.join(directory, session.username, owner, repo);
 
             request.projectWorkspace = new ProjectWorkspace(session, workspacePath, owner, repo, minitPath);
