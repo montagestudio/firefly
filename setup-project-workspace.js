a63d0ae09f6534879e2902b566e1069a82d79fd7
Stuart Knightley <stuart@stuartk.com>
2013-11-22 15:03:12 -0800

    Create user/owner clone directory if it does not exist

--- setup-project-workspace.js
+++ setup-project-workspace.js
@@ -1,3 +1,4 @@
+var log = require("logging").from(__filename);
 var ProjectWorkspace = require("./project-workspace");
 var sanitize = require("./sanitize");
 var fs = require("q-io/fs");
@@ -7,14 +8,24 @@ module.exports = SetupProjectWorkspace;
 function SetupProjectWorkspace(directory, minitPath) {
     return function(next) {
         return function(request, response) {
-            var session = request.session,
-                owner = sanitize.sanitizeDirectoryName(request.params.owner).toLowerCase(),
-                repo = sanitize.sanitizeDirectoryName(request.params.repo).toLowerCase(),
-                workspacePath = fs.join(directory, session.username, owner, repo);
+            var session = request.session;
+            var owner = sanitize.sanitizeDirectoryName(request.params.owner).toLowerCase();
+            var repo = sanitize.sanitizeDirectoryName(request.params.repo).toLowerCase();
 
-            request.projectWorkspace = new ProjectWorkspace(session, workspacePath, owner, repo, minitPath);
+            var userOwnerPath = fs.join(directory, session.username, owner);
+            var workspacePath = fs.join(userOwnerPath, repo);
 
-            return next(request, response);
+            return fs.exists(userOwnerPath)
+            .then(function (exists) {
+                if (!exists) {
+                    log("creating user/owner directory:", userOwnerPath);
+                    return fs.makeTree(userOwnerPath, "700"); // -rwx------
+                }
+            })
+            .then(function () {
+                request.projectWorkspace = new ProjectWorkspace(session, workspacePath, owner, repo, minitPath);
+                return next(request, response);
+            });
         };
     };
 }
