f3137be4ab31fbe00d5b01e8c4d2d79ffce6cefc
Stuart Knightley <stuart@stuartk.com>
2013-11-25 15:33:23 -0800

    Fix tests based on dev/prod unification

--- spec/environment-spec.js
+++ spec/environment-spec.js
@@ -6,32 +6,18 @@ describe("environment", function () {
         beforeEach(function () {
             environment = Env();
         });
-        describe("getProjectUrlFromAppUrl", function () {
+
+        describe("getAppUrl", function () {
             it("returns a url", function () {
-                expect(environment.getProjectUrlFromAppUrl("owner/repo")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2441");
-                expect(environment.getProjectUrlFromAppUrl("/owner/repo")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2441");
-                expect(environment.getProjectUrlFromAppUrl("/owner/repo/fail")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2441");
+                expect(environment.getAppUrl()).toEqual("http://local-firefly.declarativ.net:2440");
             });
         });
-    });
-    describe("in production", function () {
-        beforeEach(function () {
-            environment = Env({  production: true });
-        });
+
         describe("getProjectUrlFromAppUrl", function () {
             it("returns a url", function () {
-                expect(environment.getProjectUrlFromAppUrl("owner/repo")).toEqual("http://p0.project.127.0.0.1.xip.io");
-                expect(environment.getProjectUrlFromAppUrl("/owner/repo2")).toEqual("http://p1.project.127.0.0.1.xip.io");
-                expect(environment.getProjectUrlFromAppUrl("/owner/repo3/fail")).toEqual("http://p2.project.127.0.0.1.xip.io");
-            });
-            it("can be recovered from the project domain", function () {
-                var detail = environment.getDetailsfromProjectUrl(environment.getProjectUrlFromAppUrl("owner/repo"));
-                expect(detail.owner).toEqual("owner");
-                expect(detail.repo).toEqual("repo");
-            });
-            it("should return a stable url", function () {
-                expect(environment.getProjectUrlFromAppUrl("owner/repo")).toEqual("http://p0.project.127.0.0.1.xip.io");
-                expect(environment.getProjectUrlFromAppUrl("/owner/repo")).toEqual("http://p0.project.127.0.0.1.xip.io");
+                expect(environment.getProjectUrlFromAppUrl("owner/repo")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2440");
+                expect(environment.getProjectUrlFromAppUrl("/owner/repo")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2440");
+                expect(environment.getProjectUrlFromAppUrl("/owner/repo/fail")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2440");
             });
         });
     });
