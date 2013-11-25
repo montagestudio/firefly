116012acb33b1103d19bd3775a4395a84f850da7
Stuart Knightley <stuart@stuartk.com>
2013-11-25 14:58:18 -0800

    Server is normalized between dev and production

--- environment.js
+++ environment.js
@@ -2,11 +2,22 @@ var URL = require("url");
 
 function Env(options) {
     var env = options || {  production: process.env.NODE_ENV === "production" };
+
+    env.app = URL.parse(process.env.FIREFLY_APP_URL || "http://local-firefly.declarativ.net:2440");
+    // Remove `host` so that `URL.format` uses `hostname` and `port` instead.
+    // Remove `pathname` so that `URL.format` doesn't add "/" to the end,
+    // messing up the CORS Accept-Origin header.
+    delete env.app.host;
+    delete env.app.pathname;
+
+    env.project = URL.parse(process.env.FIREFLY_PROJECT_URL || "http://project.127.0.0.1.xip.io:2440");
+    delete env.project.host;
+    delete env.project.pathname;
+
     env.getAppUrl = function () {
         return URL.format(this.app);
     };
 
-    //App works the same on dev and production
     env.getDetailsFromAppUrl = function (url) {
         var pathname = URL.parse(url).pathname;
 
@@ -27,162 +38,60 @@ function Env(options) {
         return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
     };
 
-    // PRODUCTION
-    if ( env.production) {
-        env.app = {
-            hostname: process.env.FIREFLY_APP_HOST || "app.127.0.0.1.xip.io",
-            protocol: process.env.FIREFLY_APP_PROTOCOL || "http"
-        };
-        env.project = {
-            hostname: process.env.FIREFLY_PROJECT_HOST || "*.project.127.0.0.1.xip.io",
-            protocol: process.env.FIREFLY_PROJECT_PROTOCOL || "http"
-        };
+    env.getDetailsFromAppUrl = function (url) {
+        var pathname = URL.parse(url).pathname;
 
-        var projectList = [];
-        var projectMemory = {};
-        var projectIndex = -1;
-        var projectInMemory = function addProject(project) {
-            // first check if we already have a number
-            return projectMemory[project.owner + "-" + project.repo];
+        var match = pathname.match(/\/?([^\/]+)\/([^\/]+)/);
+        var owner = match[1];
+        var repo = match[2];
+
+        return {
+            owner: owner.toLowerCase(),
+            repo: repo.toLowerCase()
         };
-        var removeProjectInMemory = function addProject(project) {
-            delete projectMemory[project.owner + "-" + project.repo] ;
-        };
-        var addProjectInMemory = function addProject(project, number) {
-            projectMemory[project.owner + "-" + project.repo] = number;
-        };
-
-        var getProjectNumber = function addProject(project) {
-            // first check if we already have a number
-            var projectNumber = projectInMemory(project);
-
-            if (projectNumber != null) {
-                return projectNumber;
-            }
-
-            projectIndex++;
-            if(projectIndex > 40) {
-                projectIndex = 0;
-            }
-            // remove old one if one exists
-            removeProjectInMemory(project);
-            projectList[projectIndex] = project;
-            // add new one if one exists
-            addProjectInMemory(project, projectIndex);
-            return projectIndex;
-        };
-        env.getDetailsfromProjectUrl = function (url) {
-            // Needed because node's URL.parse interprets
-            // a.b.c.com:1234
-            // as
-            // protocol: "a.b.c.d.com"
-            // hostname: "2440"
-            if (!/^https?:\/\//.test(url)) {
-                url = "http://" + url;
-            }
-
-            var number = URL.parse(url).hostname.match(/p(\d+)\./i)[1];
-
-            var project = projectList[parseInt(number, 10)];
-
-            return {
-                owner: project.owner.toLowerCase(),
-                repo: project.repo.toLowerCase()
-            };
-        };
-        env.getProjectUrlFromAppUrl = function (url) {
-            var details = this.getDetailsFromAppUrl(url);
-            var urlObj = Object.create(this.project);
-            urlObj.hostname = "p" + getProjectNumber(details) + urlObj.hostname.substring(1, urlObj.hostname.length);
-            return URL.format(urlObj);
-        };
-        env.getProjectPathFromSessionAndProjectUrl = function (session, url) {
-            var details = this.getDetailsfromProjectUrl(url);
-
-            // FIXME not to use FS
-            var FS = require("q-io/fs");
-            return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
-        };
-
-    } else {
-
-        env.app = {
-            hostname: "app.127.0.0.1.xip.io",
-            port: 2440,
-            protocol: "http"
-        };
-        env.project = {
-            hostname: "project.127.0.0.1.xip.io",
-            port: 2441,
-            protocol: "http"
-        };
-
-        if (process.env.NODE_ENV === "semiproduction") {
-            env.app = {
-                hostname: process.env.FIREFLY_APP_HOST,
-                protocol: "http"
-            };
-            env.project = {
-                hostname: process.env.FIREFLY_PROJECT_HOST,
-                protocol: "http",
-                port: 2441
-            };
+    };
+    env.getDetailsfromProjectUrl = function (url) {
+        // Needed because node's URL.parse interprets
+        // a.b.c.com:1234
+        // as
+        // protocol: "a.b.c.d.com"
+        // hostname: "2440"
+        if (!/^https?:\/\//.test(url)) {
+            url = "http://" + url;
         }
+        var hostname = URL.parse(url).hostname;
 
-        env.getDetailsFromAppUrl = function (url) {
-            var pathname = URL.parse(url).pathname;
+        var match = hostname.match(/([a-z\-]+)-([a-z]+)\./i);
+        var owner = match[1];
+        var repo = match[2];
 
-            var match = pathname.match(/\/?([^\/]+)\/([^\/]+)/);
-            var owner = match[1];
-            var repo = match[2];
-
-            return {
-                owner: owner.toLowerCase(),
-                repo: repo.toLowerCase()
-            };
+        return {
+            owner: owner.toLowerCase(),
+            repo: repo.toLowerCase()
         };
-        env.getDetailsfromProjectUrl = function (url) {
-            // Needed because node's URL.parse interprets
-            // a.b.c.com:1234
-            // as
-            // protocol: "a.b.c.d.com"
-            // hostname: "2440"
-            if (!/^https?:\/\//.test(url)) {
-                url = "http://" + url;
-            }
-            var hostname = URL.parse(url).hostname;
+    };
+    env.getProjectUrlFromAppUrl = function (url) {
+        var details = this.getDetailsFromAppUrl(url);
+        var urlObj = Object.create(this.project);
+        urlObj.hostname = details.owner + "-" + details.repo + "." + urlObj.hostname;
+        return URL.format(urlObj);
+    };
+    env.getProjectPathFromSessionAndAppUrl = function (session, url) {
+        var details = this.getDetailsFromAppUrl(url);
 
-            var match = hostname.match(/([a-z\-]+)-([a-z]+)\./i);
-            var owner = match[1];
-            var repo = match[2];
+        // FIXME not to use FS
+        var FS = require("q-io/fs");
+        return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
+    };
+    env.getProjectPathFromSessionAndProjectUrl = function (session, url) {
+        var details = this.getDetailsfromProjectUrl(url);
 
-            return {
-                owner: owner.toLowerCase(),
-                repo: repo.toLowerCase()
-            };
-        };
-        env.getProjectUrlFromAppUrl = function (url) {
-            var details = this.getDetailsFromAppUrl(url);
-            var urlObj = Object.create(this.project);
-            urlObj.hostname = details.owner + "-" + details.repo + "." + urlObj.hostname;
-            return URL.format(urlObj);
-        };
-        env.getProjectPathFromSessionAndAppUrl = function (session, url) {
-            var details = this.getDetailsFromAppUrl(url);
+        // FIXME not to use FS
+        var FS = require("q-io/fs");
+        return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
+    };
 
-            // FIXME not to use FS
-            var FS = require("q-io/fs");
-            return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
-        };
-        env.getProjectPathFromSessionAndProjectUrl = function (session, url) {
-            var details = this.getDetailsfromProjectUrl(url);
-
-            // FIXME not to use FS
-            var FS = require("q-io/fs");
-            return FS.join(process.cwd(), "..", "clone", session.username, details.owner, details.repo);
-        };
-
-    }
+    // }
     return env;
 }
 
--- multiplex.js
+++ multiplex.js
@@ -1,191 +1,48 @@
-var Env = require("./environment");
+var log = require("logging").from(__filename);
+var environment = require("./environment");
 var Q = require("q");
 var joey = require("joey");
 var Status = require("q-io/http-apps/status");
 
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
 
 module.exports = multiplex;
 function multiplex(options, appChainFactory, appChainOptions, projectChainFactory, projectChainOptions) {
-
-    if(Env.production) {
-        return Q.all([
-                appChainFactory(appChainOptions),
-                projectChainFactory(projectChainOptions)
-            ])
-            .spread(function (appChain, projectChain) {
-
-                var contraint = Constrain(Host).call(joey, Env.app.hostname+":*", Env.project.hostname+":*");
-
-                var multiplex = Multiplex(Host).call(contraint, function (branch) {
-                    branch(Env.app.hostname+":*").app(appChain.end());
-                    branch(Env.project.hostname+":*").app(projectChain.end());
-                });
-                return multiplex
-                    .listen(process.env.FIREFLY_APP_PORT)
-                    .then(function (server) {
-                        return [
-                            {
-                                chain: appChain,
-                                server: server
-                            },
-                            {
-                                chain: projectChain,
-                                server: server
-                            }
-                        ];
-                    });
-
-//                return joey
-//                    .host(Env.app.hostname+":*", Env.project.hostname+":*")
-//                    .hosts(function (branch) {
-//                        branch(Env.app.hostname+":*").app(appChain.end());
-//                        branch(Env.project.hostname+":*").app(projectChain).end();
-//                    })
-//                    .listen(Env.app.port)
-//                    .then(function (server) {
-//                        return [
-//                            {
-//                                chain: appChain,
-//                                server: server
-//                            },
-//                            {
-//                                chain: projectChain,
-//                                server: server
-//                            }
-//                        ];
-//                    });
-            });
-    } else {
-        return Q.all([
-            appChainFactory(appChainOptions)
-            .then(function (chain) {
-                return chain.listen(options["app-port"] || 2440).then(function (server) {
-                    return {
-                        chain: chain,
-                        server: server
-                    };
-                });
-            }),
-            projectChainFactory(projectChainOptions)
-            .then(function (chain) {
-                return chain.listen(options["project-port"]).then(function (server) {
-                    return {
-                        chain: chain,
-                        server: server
-                    };
-                });
-            })
-        ]);
-    }
-
-
-    // TODO: multiplex based on request.headers.host, instead of starting
-    // two servers on different ports
-}
-
-
-var Host = function (appForHost, notAcceptable) {
-    var table = Object.keys(appForHost).map(function (pattern) {
-        var parts = pattern.split(":");
-        var hostname = parts[0];
-        if(hostname) {
-            // store part in reverse order
-            var hostnameParts = hostname.split(".");
-            hostname = [];
-            var i = hostnameParts.length;
-            while(i--) {
-                hostname.push(hostnameParts[i]);
-            }
-        }
-        return [
-            pattern,
-            hostname || ["*"],
-            parts[1] || "*",
-            appForHost[pattern]
-        ];
-    });
-    if (!notAcceptable) {
-        notAcceptable = Status.notAcceptable;
-    }
-    return function (request, response) {
-        var matchHostname = function(hostname, requestHostname) {
-            var hostnameParts = requestHostname.split(":")[0].split(".");
-            var hostnameIndex = 0;
-            var i = hostnameParts.length;
-            var isMatch = true;
-            while(i--) {
-                 // check starting
-                var part = hostname[hostnameIndex];
-                if(part) {
-                    if ( part === "*") {
-                        break;
-                    } else if( part !== hostnameParts[i]) {
-                        isMatch = false;
-                        break;
-                    }
+    return Q.all([
+        appChainFactory(appChainOptions),
+        projectChainFactory(projectChainOptions)
+    ])
+    .spread(function (appChain, projectChain) {
+        var appHandler = appChain.end();
+        var projectHandler = projectChain.end();
+        return joey.use(function (next) {
+            return function (request) {
+                var hostname = request.hostname;
+                if (endsWith(hostname, environment.app.hostname)) {
+                    return appHandler(request);
+                } else if (endsWith(hostname, environment.project.hostname)) {
+                    return projectHandler(request);
                 } else {
-                    isMatch = false;
-                    break;
+                    log("*Unrecognized hostname*", hostname);
+                    return Status.notAcceptable(request);
                 }
-                hostnameIndex++;
-            }
-            return isMatch;
-        };
-        // find first matching host for app
-        for (var index = 0; index < table.length; index++) {
-            var row = table[index]; // [hostname, port, app]
-            var pattern = row[0];
-            var hostname = row[1];
-            var port = row[2];
-            var app = row[3];
-            if (
-                matchHostname(hostname, request.hostname) &&
-                (port === "*" || port === "" + request.port)
-            ) {
-                if (!request.terms) {
-                    request.terms = {};
+            };
+        })
+        // .listen(process.env.FIREFLY_APP_PORT)
+        .listen(2440)
+        .then(function (server) {
+            return [
+                {
+                    chain: appChain,
+                    server: server
+                },
+                {
+                    chain: projectChain,
+                    server: server
                 }
-                request.terms.host = pattern;
-                return app(request, response);
-            }
-        }
-        return notAcceptable(request, response);
-    };
-};
-
-
-//Copied from joey
-
-var Multiplex = function (Using) {
-    return function (setup) {
-        var branches = {};
-        var chain = this;
-        setup(function () {
-            var branch = new chain.constructor();
-            Array.prototype.forEach.call(arguments, function (name) {
-                branches[name] = branch;
-            });
-            return branch;
+            ];
         });
-        return this.use(function (next) {
-            Object.keys(branches).forEach(function (name) {
-                var branch = branches[name];
-                branches[name] = branch.end();
-            });
-            return Using(branches, next);
-        });
-    };
-};
-
-var Constrain = function (Using) {
-    return function () {
-        var args = arguments;
-        var types = {};
-        return this.use(function (next) {
-            Array.prototype.forEach.call(args, function (type) {
-                types[type] = next;
-            });
-            return Using(types);
-        });
-    };
-};
+    });
+}
