ca03f59ae27ae45098d4c9c9c7c1fe77f4f79ce5
Stuart Knightley <stuart@stuartk.com>
2013-12-02 16:58:34 -0800

    Fix tests

--- spec/environment-spec.js
+++ spec/environment-spec.js
@@ -15,9 +15,9 @@ describe("environment", function () {
 
         describe("getProjectUrlFromAppUrl", function () {
             it("returns a url", function () {
-                expect(environment.getProjectUrlFromAppUrl("owner/repo")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2440");
-                expect(environment.getProjectUrlFromAppUrl("/owner/repo")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2440");
-                expect(environment.getProjectUrlFromAppUrl("/owner/repo/fail")).toEqual("http://owner-repo.project.127.0.0.1.xip.io:2440");
+                expect(environment.getProjectUrlFromAppUrl("owner/repo")).toEqual("http://owner-repo.local-project.declarativ.net:2440");
+                expect(environment.getProjectUrlFromAppUrl("/owner/repo")).toEqual("http://owner-repo.local-project.declarativ.net:2440");
+                expect(environment.getProjectUrlFromAppUrl("/owner/repo/fail")).toEqual("http://owner-repo.local-project.declarativ.net:2440");
             });
         });
     });
