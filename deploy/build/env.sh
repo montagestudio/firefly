9a8ff61b90d880a96e4eca351c2b19de870581d6
Stuart Knightley <stuart@stuartk.com>
2014-01-13 19:17:36 -0800

    Rename temporary directory .deploy

--- deploy/build/env.sh
+++ deploy/build/env.sh
@@ -6,7 +6,7 @@ HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Root is 2 directories up from here
 export HOME="$(dirname $(dirname ${HOME}))"
 
-export BUILD=${HOME}/.install
+export BUILD=${HOME}/.deploy
 
 export GEM_HOME=${BUILD}/digitalocean
 export GEM_PATH=${GEM_HOME}/gems:/Library/Ruby/Gems/2.0.0
