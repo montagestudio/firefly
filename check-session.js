3d41c698f2276c1ce54164914e04470d57b67360
Jean-Francois Ducarroz <github@ducarroz.org>
2014-01-14 12:40:57 -0800

    Fix for FLY-36: Store User's Github authentication token on client side
    By using the github authentication token and username as session id allow us to restore a session after a server restart. This will also allow the user to be connected to more than one server without having to authenticate again.

--- check-session.js
+++ check-session.js
@@ -1,20 +1,53 @@
 var env = require("./environment");
+var Q = require("q");
+var GithubApi = require("./inject/adaptor/client/core/github-api");
 
 exports = module.exports = CheckSession;
 
-function CheckSession(next) {
-    return function(request, response) {
-        var user = request.session.githubUser;
+function CheckSession(key) {
+    return function(next) {
+        return function(request, response) {
+            var user = request.session.githubUser;
 
-        if (user) {
-            return next(request, response);
-        } else {
-            return {
-                status: 302,
-                headers: {
-                    "Location": env.getAppUrl()
+            if (user) {
+                return next(request, response);
+            } else {
+                // Restore the session if the session id is a valid github token
+                var done = Q();
+                var _validUser = false;
+                var sessionID = request.cookies[key];
+                var githubLoginInfo = sessionID.match(/^([0-9a-f]+)\/(.+)/);
+
+                if (githubLoginInfo && githubLoginInfo.length === 3) {
+                    var githubApi = new GithubApi(githubLoginInfo[1]);
+
+                    done = githubApi.getUser().then(function(user) {
+                        var username = user.login.toLowerCase();
+
+                        if (githubLoginInfo[2] === username) {
+                            request.session.sessionId = sessionID;
+                            request.session.githubUser = user;
+                            request.session.username = user.login.toLowerCase();
+                            request.session.githubAccessToken = githubLoginInfo[1];
+
+                            _validUser = true;
+                        }
+                    });
                 }
-            };
-        }
+
+                return done.then(function() {
+                    if (_validUser) {
+                        return next(request, response);
+                    } else {
+                        return {
+                            status: 302,
+                            headers: {
+                                "Location": env.getAppUrl()
+                            }
+                        };
+                    }
+                });
+            }
+        };
     };
 }
