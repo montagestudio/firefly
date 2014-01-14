089197df5f06e04db782a0dff89a08a5497d70c9
Stuart Knightley <stuart@stuartk.com>
2014-01-13 16:22:36 -0800

    Rename deploy packer json files

--- deploy/build/base-image.sh
+++ deploy/build/base-image.sh
@@ -6,4 +6,4 @@ ${BUILD}/packerio/packer build \
 	-var "do_api_key=3b6311afca5bd8aac647b316704e9c6d" \
 	-var "do_client_id=383c8164d4bdd95d8b1bfbf4f540d754" \
 	-var "snapshot_name=baseimage" \
-	${HOME}/deploy/baseimage.json
+	${HOME}/deploy/base-image.json
