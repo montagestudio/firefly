0bcaf0b9945102daabca6f4bb13f9f7af87dd533
António Afonso <antonio.afonso@gmail.com>
2014-01-15 16:14:55 -0800

    Check preview access and redirect to access form
    Instead of only granting access to the authenticated user to its own
    projects also allow access to users who have been granted access through
    an access code.

---
+++ preview/check-preview-access.js
@@ -0,0 +1,46 @@
+var env = require("../environment");
+var PreviewServer = require("./preview-server");
+
+exports = module.exports = CheckPreviewAccess;
+exports.hasPreviewAccess = hasPreviewAccess;
+
+function CheckPreviewAccess(next) {
+    return function(request, response) {
+        var host = request.headers.host;
+        var hasAccess = hasPreviewAccess(host, request.session);
+
+        if (hasAccess) {
+            return next(request, response);
+        } else {
+            return PreviewServer.servePreviewAccessForm(request);
+        }
+    };
+}
+
+/**
+ * The owner always has access to its own projects.
+ * All other users will only have access if they have been authenticated to see
+ * this preview and the owner has it open in the tool.
+ */
+function hasPreviewAccess(url, session) {
+    var hasAccess = false;
+
+    if (session) {
+        var user = session.githubUser;
+        var details = env.getDetailsfromProjectUrl(url);
+        // The user doesn't need to have explicit access to its own previews.
+        if (user && user.login === details.owner) {
+            hasAccess = true;
+        }
+        // No reason to give a random user access to the preview if the owner
+        // doesn't have it open in the tool.
+        else if (preview.existsPreviewFromUrl(url)) {
+            var previewAccess = session.previewAccess;
+            if (previewAccess && previewAccess.indexOf(url) >= 0) {
+                hasAccess = true;
+            }
+        }
+    }
+
+    return hasAccess;
+}
---
+++ spec/check-preview-access-spec.js
@@ -0,0 +1,84 @@
+var Q = require("q");
+var CheckPreviewAccess = require("../preview/check-preview-access");
+var PreviewService = require("../services/preview-service");
+
+describe("check-preview-access", function () {
+    var chain,
+        checkPreviewAccess,
+        host = "http://owner-repo.local-project.127.0.0.1.xip.io:2440",
+        request,
+        previewService = new PreviewService.service();
+
+    beforeEach(function () {
+        request = {
+            session: {
+                githubUser: {login: "owner"},
+                previewAccess: []
+            },
+            headers: {host: host}
+        };
+        PreviewService.unregisterAllConnections();
+
+        // Q.bind() in this case will create a function that returns a resolved
+        // promise.
+        chain = {next: Q.bind()};
+        spyOn(chain, "next").andCallThrough();
+        checkPreviewAccess = CheckPreviewAccess(chain.next);
+    });
+
+    it("should grant access to the logged user to its own previews", function(done) {
+        checkPreviewAccess(request)
+        .then(function() {
+            expect(chain.next).toHaveBeenCalled();
+        }).then(done, done);
+    });
+
+    it("should not grant access when the user doesn't have access and preview isn't registered", function(done) {
+        request.session.githubUser.login = "user";
+
+        checkPreviewAccess(request)
+        .then(function() {
+            expect(chain.next).not.toHaveBeenCalled();
+        }).then(done, done);
+    });
+
+    it("should not grant access when the user doesn't have access and preview is registered", function(done) {
+        previewService.register({name: "", url: host});
+        request.session.githubUser.login = "user";
+
+        checkPreviewAccess(request)
+        .then(function() {
+            expect(chain.next).not.toHaveBeenCalled();
+        }).then(done, done);
+    });
+
+    it("should not grant access when the user has access and the preview isn't registered", function(done) {
+        request.session.githubUser.login = "user";
+        request.session.previewAccess.push(host);
+
+        checkPreviewAccess(request)
+        .then(function() {
+            expect(chain.next).not.toHaveBeenCalled();
+        }).then(done, done);
+    });
+
+    it("should grant access when the user has access and a preview is registered", function(done) {
+        previewService.register({name: "", url: host});
+        request.session.githubUser.login = "user";
+        request.session.previewAccess.push(host);
+
+        checkPreviewAccess(request)
+        .then(function() {
+            expect(chain.next).toHaveBeenCalled();
+        }).then(done, done);
+    });
+
+    it("should serve the preview access form when the user isn't granted access", function(done) {
+        request.session.githubUser.login = "user";
+
+        checkPreviewAccess(request)
+        .then(function(response) {
+            expect(response.file).toBe("access.html");
+        }).then(done, done);
+    });
+});
