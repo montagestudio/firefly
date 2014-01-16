c32a4c9160637c806bf7fee575382a0c6374ecd5
Stuart Knightley <stuart@stuartk.com>
2014-01-16 13:48:06 -0800

    Pull bash `get` function into its own file

--- deploy/build/project-image.sh
+++ deploy/build/project-image.sh
@@ -2,50 +2,7 @@
 
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/env.sh"
 
-if [[ $GITHUBDECLARATIV == "" ]]; then
-    export GITHUBDECLARATIV="github.com"
-fi
-
-get ()
-{
-  # $1 is directory
-  # $2 is commit hash
-
-  rm -rf ${BUILD}/$1
-  git clone git@$GITHUBDECLARATIV:declarativ/$1.git ${BUILD}/$1
-  if [[ -e ${BUILD}/$1 ]]; then
-    pushd ${BUILD}/$1
-        git config user.name "Declarativ Bot"
-        git config user.email dev@declarativ.com
-
-        # if commit hash is set then check it out
-        if [ "$2" ]; then
-            git checkout "$2"
-        fi
-
-        # Tag every deploy
-        # git tag -a -m "$BUILD_URL" "deploy-$BUILD_NUMBER"
-        # git push --tags
-
-        # Create a file with the git hash so that given just an image we know what
-        # code is in it
-        git rev-parse HEAD > GIT_HASH
-
-        # Remove git directory before uploading to server to reduce size
-        rm -rf .git
-
-        # Remove test code
-        rm -rf test
-        rm -rf spec
-    popd
-    pushd ${BUILD}
-        tar -czf $1.tgz $1
-    popd
-  else
-      echo "Cannot clone git repository: "${BUILD}/$1
-      exit -1
-  fi
-}
+source "${HOME}/deploy/build/get.sh"
 
 get filament $FILAMENT_COMMIT
 get firefly $FIREFLY_COMMIT
@@ -64,5 +21,5 @@ ${BUILD}/packerio/packer build \
     -var "snapshot_name=projectimage-$BUILD_NUMBER" \
     ${HOME}/deploy/project-image.json
 
-rm -rf ${BUILD}/firefly
-rm -rf ${BUILD}/firefly.tgz
+  get-clean filament
+  get-clean firefly
