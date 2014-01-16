9c02b61eb3fe821f9f64357284fae06ea8a92bcf
Stuart Knightley <stuart@stuartk.com>
2014-01-16 13:38:33 -0800

    Fix firefly image script
    Don't need to pack the static files into filament here

--- deploy/build/firefly-image.sh
+++ deploy/build/firefly-image.sh
@@ -50,19 +50,6 @@ get ()
 get filament $FILAMENT_COMMIT
 get firefly $FIREFLY_COMMIT
 
-# Lets do a bit of cleanup
-pushd ${BUILD}
-    if [[ -e "firefly" ]]; then
-        rm -rf "firefly/deploy"
-        if [[ -e "filament" ]]; then
-            cp -R "firefly/inject/*" "filament/."
-            tar -czf "filament.tgz" "filament"
-        fi
-        tar -czf "firefly.tgz" "firefly"
-    fi
-popd
-
-
 ${BUILD}/packerio/packer build \
     -var "do_api_key=3b6311afca5bd8aac647b316704e9c6d" \
     -var "do_client_id=383c8164d4bdd95d8b1bfbf4f540d754" \
