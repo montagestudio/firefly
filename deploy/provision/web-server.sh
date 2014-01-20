090280ef5d17d52c7025bba03f640248a54a091f
Pierre Frisch <pierre.frisch@spearway.com>
2014-01-20 15:09:32 -0800

    Fix the web server configuration

--- deploy/provision/web-server.sh
+++ deploy/provision/web-server.sh
@@ -13,6 +13,9 @@ if [[ -e "/srv/filament.tgz" ]]; then
 	mv filament app
     chown -R montage:montage app
 	rm -rf filament.tgz
+	# We need to token file at th erooot to respond to the haproxy requests
+	echo "<html><\html>" > index.html
+    chown -R montage:montage index.html
 fi
 
 popd
