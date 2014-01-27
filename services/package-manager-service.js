1e9c06795bf9e035caceebea06fbf735b5352fb1
Thibault Zanini <thibaultzanini@gmail.com>
2014-01-27 15:46:43 +0100

    Add installProjectPackages
    Allows to install all missing dependency of a project.
    
    Cleanup
    
    Improvements with installProjectPackages
    
    Cleaning up

--- services/package-manager-service.js
+++ services/package-manager-service.js
@@ -3,6 +3,7 @@ var fileService = require('./file-service');
 var RemovePackage = require('../package-manager/remove-package');
 var SearchPackages = require('../package-manager/search-packages');
 var execNpm = require('../package-manager/exec-npm');
+var Q = require('q');
 
 //FIXME use fs from the service once the function “removeTree” of QFS would have be fixed after having reroot it.
 var FS = require("q-io/fs");
@@ -37,6 +38,17 @@ function PackageManagerService (fs, environment, pathname, fsPath) {
 
     service.findOutdatedDependency = function () {
         return execNpm(execNpm.COMMANDS.OUTDATED, null, fsPath);
+    };
+
+    service.installProjectPackages = function () {
+        return listDependencies(fs, fs.ROOT, true).then(function (dependencyTree) {
+            var dependenciesToInstall = dependencyTree.children.regular,
+                requestedPackages = dependenciesToInstall.filter(function (dependency) {
+                    return dependency.missing;
+                });
+
+            return execNpm(execNpm.COMMANDS.INSTALL, requestedPackages, fsPath);
+        });
     };
 
     service.searchPackages = SearchPackages;
