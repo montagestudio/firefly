88a26504bde0cfbb00f9ac4d6e2cf954225e263a
Stuart Knightley <stuart@stuartk.com>
2014-01-28 11:58:16 -0800

    Increase test timeout so it passes more reliably

--- spec/project/services/package-manager-services/view-dependency-spec.js
+++ spec/project/services/package-manager-services/view-dependency-spec.js
@@ -4,7 +4,7 @@ var execNpm = require('../../../../project/package-manager/exec-npm');
 
 describe("npm view command", function () {
 
-    it('should throw an error if the request is not valid.', function(done) {
+    it('should throw an error if the request is not valid.', function (done) {
 
         execNpm(execNpm.COMMANDS.VIEW, ["montage@1.0"], __dirname).then(function () {}, function (errorThrew) {
             expect(errorThrew.code).toEqual(3001);
@@ -13,15 +13,13 @@ describe("npm view command", function () {
 
     });
 
-    it("should get some information about montage@0.13.0.", function(done) {
-
+    it("should get some information about montage@0.13.0.", function (done) {
         execNpm(execNpm.COMMANDS.VIEW, ["montage@0.13.0"], __dirname).then(function (moduleRequested) {
             expect(typeof moduleRequested === 'object').toBeDefined();
             expect(moduleRequested.name).toEqual('montage');
             expect(moduleRequested.version).toEqual('0.13.0');
 
         }).then(done, done);
-
-    });
+    }, 10000);
 
 });
