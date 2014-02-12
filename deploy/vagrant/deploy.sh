e6c780f9ef63fb889de1c597148da76c41a4e724
Stuart Knightley <stuart@stuartk.com>
2014-02-11 16:03:47 -0800

    Update deploy script to not override environment variables
    was causing problems with npm

--- deploy/vagrant/deploy.sh
+++ deploy/vagrant/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-vagrant ssh login -c 'sudo naught deploy /home/montage/naught.ipc || (tail -n 20 /home/montage/stderr.log && exit 1)' &&
-vagrant ssh project -c 'sudo naught deploy /home/montage/naught.ipc || (tail -n 20 /home/montage/stderr.log && exit 1)'
+vagrant ssh login -c 'sudo naught deploy --override-env false /home/montage/naught.ipc || (tail -n 20 /home/montage/stderr.log && exit 1)' &&
+vagrant ssh project -c 'sudo naught deploy --override-env false /home/montage/naught.ipc || (tail -n 20 /home/montage/stderr.log && exit 1)'
