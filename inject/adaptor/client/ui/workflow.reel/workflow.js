e58eb9a923f936f04dde7a0f73ac0acc693eb4fa
Mike Czepiel <mike@czepiel.net>
2014-02-25 14:36:23 -0800

    Add git workflow component
    Nothing much to see here yet.

---
+++ inject/adaptor/client/ui/workflow.reel/workflow.js
@@ -0,0 +1,18 @@
+/**
+ * @module ui/workflow.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Workflow
+ * @extends Component
+ */
+
+exports.Workflow = Component.specialize(/** @lends Menu# */ {
+    constructor: {
+        value: function Workflow() {
+            this.super();
+        }
+    }
+});
