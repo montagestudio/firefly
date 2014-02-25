7b5424efc56722e7381ef2f9668b080e78aa87e1
Mike Czepiel <mike@czepiel.net>
2014-02-25 11:01:25 -0800

    Remove hardcoded menus from the toolbar
    This now uses the actual menu component, which still needs to be
    implemented, to display the mainMenu.

--- inject/adaptor/client/ui/toolbar.reel/toolbar.js
+++ inject/adaptor/client/ui/toolbar.reel/toolbar.js
@@ -13,19 +13,19 @@ exports.Toolbar = Component.specialize(/** @lends Toolbar# */ {
     constructor: {
         value: function Toolbar() {
             var self = this;
-
             this.super();
 
-            this.addPathChangeListener("mainMenu", this, "handleMainMenuChange");
             self.userController = new UserController().init();
-            this.addPathChangeListener("environmentBridge", function(value) {
-                if (value) {
-                    value.userController.getUser()
+            this.addPathChangeListener("environmentBridge", function(bridge) {
+                if (bridge) {
+                    self.mainMenu = bridge.mainMenu;
+
+                    bridge.userController.getUser()
                         .then(function (user) {
                             self.user = user;
                         }).done();
 
-                    value.repositoryController.getRepositoryUrl()
+                    bridge.repositoryController.getRepositoryUrl()
                     .then(function(url) {
                         self.sourceUrl = url;
                     }).done();
@@ -42,29 +42,6 @@ exports.Toolbar = Component.specialize(/** @lends Toolbar# */ {
         value: null
     },
 
-    handleMainMenuChange: {
-        value: function (menuModel) {
-            if (menuModel) {
-                this.undoMenuItemModel = menuModel.menuItemForIdentifier("undo");
-                this.redoMenuItemModel = menuModel.menuItemForIdentifier("redo");
-                this.saveMenuItemModel = menuModel.menuItemForIdentifier("save");
-                this.deleteMenuItemModel = menuModel.menuItemForIdentifier("delete");
-            }
-        }
-    },
-
-    handleNewButtonAction: {
-        value: function (event) {
-            this.environmentBridge.openHttpUrl(window.location.origin + "#new");
-        }
-    },
-
-    handleOpenButtonAction: {
-        value: function (event) {
-            this.environmentBridge.openHttpUrl(window.location.origin);
-        }
-    },
-
     handleSourceButtonAction: {
         value: function() {
             window.open(this.sourceUrl);
@@ -78,18 +55,6 @@ exports.Toolbar = Component.specialize(/** @lends Toolbar# */ {
     },
 
     sourceUrl: {
-        value: null
-    },
-
-    undoMenuItemModel: {
-        value: null
-    },
-
-    redoMenuItemModel: {
-        value: null
-    },
-
-    deleteMenuItemModel: {
         value: null
     }
 
