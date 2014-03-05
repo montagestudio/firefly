1281b510ac1fc4357e11c44c7a106b07bd605628
Stuart Knightley <stuart@stuartk.com>
2014-03-05 14:42:25 -0800

    Remove npm rebuild from web-server

--- deploy/provision/web-server.sh
+++ deploy/provision/web-server.sh
@@ -7,9 +7,6 @@ pushd /srv
 
 if [[ -e "/srv/filament.tgz" ]]; then
     tar -xzf filament.tgz
-    pushd filament
-      npm rebuild
-    popd
 	mv filament app
     chown -R montage:montage app
 	rm -rf filament.tgz
