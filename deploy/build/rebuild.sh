4eab13f5d503bc0a6b07e8722615874c27916ad0
Stuart Knightley <stuart@stuartk.com>
2014-03-06 17:28:53 -0800

    Register the deploys with the correct Rollbar project

--- deploy/build/rebuild.sh
+++ deploy/build/rebuild.sh
@@ -49,21 +49,25 @@ wait_for_droplet ()
     fi
 }
 
-ROLLBAR_ACCESS_TOKEN=afa2e8f334974bc58b0415fd06a02b40
 ROLLBAR_LOCAL_USERNAME=${BUILD_NUMBER}
 rollbar() {
+    # $1 environment
+    # $2 machine name
+    # $3 project (firefly or filament)
+    # $4 rollbar access code
+
     # We need to wait for the droplets to come back alive after rebuild
     wait_for_droplet $2
     ROLLBAR_ENVIRONMENT=$1
-    ROLLBAR_REVISION=`tugboat ssh -n $2 -q -c "cat /srv/firefly/GIT_HASH" | tail -n 1`
+    ROLLBAR_REVISION=`tugboat ssh -n $2 -q -c "cat /srv/$3/GIT_HASH" | tail -n 1`
     if [[ -z $ROLLBAR_REVISION ]]; then
         # Sometimes it does not work the first time so try again
-        ROLLBAR_REVISION=`tugboat ssh -n $2 -q -c "cat /srv/firefly/GIT_HASH" | tail -n 1`
+        ROLLBAR_REVISION=`tugboat ssh -n $2 -q -c "cat /srv/$3/GIT_HASH" | tail -n 1`
     fi
-    
+
     if [[ -n $ROLLBAR_REVISION ]]; then
          curl https://api.rollbar.com/api/1/deploy/ \
-          -F access_token=$ROLLBAR_ACCESS_TOKEN \
+          -F access_token=$4 \
           -F environment=$ROLLBAR_ENVIRONMENT \
           -F revision=$ROLLBAR_REVISION \
           -F local_username=$ROLLBAR_LOCAL_USERNAME
@@ -89,8 +93,9 @@ if [[ $PRODUCTION == "TRUE" ]]; then
     tugboat rebuild -n Project2 -m projectimage-$BUILD_NUMBER -c
     tugboat rebuild -n Project3 -m projectimage-$BUILD_NUMBER -c
     tugboat rebuild -n Project4 -m projectimage-$BUILD_NUMBER -c
-    
-    rollbar "production" "Login1"
+
+    rollbar "production" "Login1" "filament" "457750e5906f47199de4c5b51d78a141"
+    rollbar "production" "Login1" "firefly" "afa2e8f334974bc58b0415fd06a02b40"
 else
     tugboat rebuild -n StagingLoadBalancer -m loadbalancerimage-$BUILD_NUMBER -c
     tugboat rebuild -n StagingWebServer -m webserverimage-$BUILD_NUMBER -c
@@ -98,13 +103,14 @@ else
     tugboat rebuild -n StagingLogin2 -m loginimage-$BUILD_NUMBER -c
     tugboat rebuild -n StagingProject1 -m projectimage-$BUILD_NUMBER -c
     tugboat rebuild -n StagingProject2 -m projectimage-$BUILD_NUMBER -c
-    
+
     staging StagingLoadBalancer
     staging StagingWebServer
     staging StagingLogin1
     staging StagingLogin2
     staging StagingProject1
     staging StagingProject2
-    
-    rollbar "staging" "StagingLogin1"
+
+    rollbar "staging" "StagingLogin1" "filament" "457750e5906f47199de4c5b51d78a141"
+    rollbar "staging" "StagingLogin1" "firefly" "afa2e8f334974bc58b0415fd06a02b40"
 fi
