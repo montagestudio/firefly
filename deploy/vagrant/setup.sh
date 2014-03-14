7ecb963563d0dfdad593b276f0ad78d6c8153c43
Stuart Knightley <stuart@stuartk.com>
2014-03-13 17:05:05 -0700

    Add scripts and update base-image for Vagrant image
    Based on http://blog.codeship.io/2013/11/07/building-vagrant-machines-with-packer.html
    and https://github.com/flomotlik/packer-example

---
+++ deploy/vagrant/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# Install necessary libraries for guest additions and Vagrant NFS Share
+sudo apt-get -y -q install linux-headers-$(uname -r) build-essential dkms nfs-common
+
+# Install necessary dependencies
+sudo apt-get -y -q install wget
+
+# Add "vagrant" to the "admin" group to give it sudo
+usermod -a -G admin vagrant
+
+# Installing vagrant keys
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+cd ~/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+chown -R vagrant ~/.ssh
+
+# Install guest additions
+mount -o loop $HOME/VBoxGuestAdditions_*.iso /mnt
+# This will fail to install the X11 guest additions because we're headless
+yes | sh /mnt/VBoxLinuxAdditions.run || true
+umount /mnt
