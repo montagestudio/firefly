50bf938748d3a45891e5d713da80f4d3a68964b3
Pierre Frisch <pierre.frisch@spearway.com>
2014-03-21 15:07:35 -0700

    Revert "Change the default preview from https to http"
    This reverts commit f40048f8d33228379de161728a6416f3dc0910e1.

--- deploy/build/staging.sh
+++ deploy/build/staging.sh
@@ -23,7 +23,7 @@ if [[ -e /etc/init/firefly-login.conf ]]; then
     sudo sed -i.bak 's/export GITHUB_CLIENT_ID=.*/export GITHUB_CLIENT_ID="0f96f18f7f6bbc1d9ce8"/' /etc/init/firefly-login.conf
     sudo sed -i.bak 's/export GITHUB_CLIENT_SECRET=.*/export GITHUB_CLIENT_SECRET="b1150aa26598295d6ca75fc651943def8954ab44"/' /etc/init/firefly-login.conf
     sudo sed -i.bak 's/export FIREFLY_APP_URL=.*/export FIREFLY_APP_URL="https:\/\/staging-aurora.montagestudio.com"/' /etc/init/firefly-login.conf
-    sudo sed -i.bak 's/export FIREFLY_PROJECT_URL=.*/export FIREFLY_PROJECT_URL="http:\/\/staging-project.montagestudio.net"/' /etc/init/firefly-login.conf
+    sudo sed -i.bak 's/export FIREFLY_PROJECT_URL=.*/export FIREFLY_PROJECT_URL="https:\/\/staging-project.montagestudio.net"/' /etc/init/firefly-login.conf
     sudo sed -i.bak 's/export FIREFLY_PROJECT_SERVER_COUNT=.*/export FIREFLY_PROJECT_SERVER_COUNT=2/' /etc/init/firefly-login.conf
 
     sudo service firefly-login stop
@@ -35,7 +35,7 @@ if [[ -e /etc/init/firefly-project.conf ]]; then
     sudo sed -i.bak 's/export GITHUB_CLIENT_ID=.*/export GITHUB_CLIENT_ID="0f96f18f7f6bbc1d9ce8"/' /etc/init/firefly-project.conf
     sudo sed -i.bak 's/export GITHUB_CLIENT_SECRET=.*/export GITHUB_CLIENT_SECRET="b1150aa26598295d6ca75fc651943def8954ab44"/' /etc/init/firefly-project.conf
     sudo sed -i.bak 's/export FIREFLY_APP_URL=.*/export FIREFLY_APP_URL="https:\/\/staging-aurora.montagestudio.com"/' /etc/init/firefly-project.conf
-    sudo sed -i.bak 's/export FIREFLY_PROJECT_URL=.*/export FIREFLY_PROJECT_URL="http:\/\/staging-project.montagestudio.net"/' /etc/init/firefly-project.conf
+    sudo sed -i.bak 's/export FIREFLY_PROJECT_URL=.*/export FIREFLY_PROJECT_URL="https:\/\/staging-project.montagestudio.net"/' /etc/init/firefly-project.conf
     sudo sed -i.bak 's/export FIREFLY_PROJECT_SERVER_COUNT=.*/export FIREFLY_PROJECT_SERVER_COUNT=2/' /etc/init/firefly-project.conf
 
     sudo service firefly-project stop
