e0a4b8b14644bde1967dd46bad3f32a2a0f37f85
Stuart Knightley <stuart@stuartk.com>
2014-03-27 10:55:11 -0700

    Add logging module which scrubs access codes

---
+++ logging.js
@@ -0,0 +1,35 @@
+var logging = require("logging");
+
+var SCRUB = [
+    {match: /[a-f0-9]+:x-oauth-basic/ig, replace: "SCRUBBED:x-oauth-basic"}
+];
+var SCRUB_LENGTH = SCRUB.length;
+
+var OPTIONS = {
+    filter: function (message) {
+        for (var i = 0; i < SCRUB_LENGTH; i++) {
+            message = message.replace(SCRUB[i].match, SCRUB[i].replace);
+        }
+        return message;
+    }
+};
+
+// Mainly here for testability, but used to create exports.from as well
+exports.makeFrom = function (givenOptions) {
+    var options = OPTIONS;
+
+    //jshint -W089
+    if (givenOptions) {
+        options = Object.create(OPTIONS);
+        for (var p in givenOptions) {
+            options[p] = givenOptions[p];
+        }
+    }
+    //jshint +W089
+
+    return function from(path) {
+        return logging.from(path, options);
+    };
+};
+
+exports.from = exports.makeFrom();
