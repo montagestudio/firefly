4b589c7af8aa75e9eeba16ff2ebbc46bb264799d
Stuart Knightley <stuart@stuartk.com>
2014-03-26 17:56:05 -0700

    Don't need to npm rebuild on deploy
    Checked in modules are build for Linux

--- deploy/provision/login.sh
+++ deploy/provision/login.sh
@@ -6,16 +6,10 @@ pushd /srv
 
 if [[ -e "/srv/filament.tgz" ]]; then
     tar -xzf filament.tgz
-    pushd filament
-      npm rebuild
-    popd
     chown -R montage:montage filament
 fi
 if [[ -e "/srv/firefly.tgz" ]]; then
     tar -xzf firefly.tgz
-    pushd firefly
-      npm rebuild
-    popd
     chown -R montage:montage firefly
 fi
 
