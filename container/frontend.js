c597984cae9b2d8692e4bd89187604be8f3035d7
Jean-Francois Ducarroz <github@ducarroz.org>
2014-04-03 11:02:51 -0700

    Add Frontend.dispatchAppEventNamed

--- container/frontend.js
+++ container/frontend.js
@@ -20,8 +20,8 @@ module.exports = {
 
         frontends[frontendId] = new Frontend(connection);
 
-        promises = this._notificationsQueue.map(function(message) {
-            return this.showNotification(message);
+        promises = this._notificationsQueue.map(function(notification) {
+            return notification.fn.apply(this, notification.args);
         }, this);
         this._notificationsQueue.clear();
 
@@ -37,13 +37,26 @@ module.exports = {
         var frontendKeys = Object.keys(frontends);
 
         if (frontendKeys.length === 0) {
-            this._notificationsQueue.push(message);
+            this._notificationsQueue.push({fn:this.showNotification, args:[message]});
         }
 
         return Q.all(frontendKeys.map(function (id) {
             return frontends[id].showNotification(message);
         })).thenResolve();
+    },
+
+    dispatchAppEventNamed: function (type, canBubble, cancelable, detail) {
+        var frontendKeys = Object.keys(frontends);
+
+        if (frontendKeys.length === 0) {
+            this._notificationsQueue.push({fn:this.dispatchAppEventNamed, args:[type, canBubble, cancelable, detail]});
+        }
+
+        return Q.all(frontendKeys.map(function (id) {
+            return frontends[id].dispatchAppEventNamed(type, canBubble, cancelable, detail);
+        })).thenResolve();
     }
+
 };
 
 function Frontend(connection) {
@@ -55,6 +68,16 @@ Frontend.prototype.showNotification = function(message) {
         return this._connection.invoke("showNotification", message);
     } else {
         log("showNotification: frontend service is not available yet");
+        return Q.resolve();
+    }
+};
+
+
+Frontend.prototype.dispatchAppEventNamed = function(type, canBubble, cancelable, detail) {
+    if (this._connection) {
+        return this._connection.invoke("dispatchAppEventNamed", type, canBubble, cancelable, detail);
+    } else {
+        log("dispatchAppEventNamed: frontend service is not available yet");
         return Q.resolve();
     }
 };
