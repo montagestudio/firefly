2c92f34ea0a4b0a7d18489a4a0bc012abccb61b2
Stuart Knightley <stuart@stuartk.com>
2014-04-04 11:25:50 -0700

    Correct error messages when things go wrong in the container

--- project/docker-container.js
+++ project/docker-container.js
@@ -15,29 +15,33 @@ function Container(modem, id) {
 }
 
 Container.prototype.start = function () {
+    var self = this;
     return Q.npost(this.container, "start", arguments)
     .catch(function (error) {
-        throw new Error("Could not start container " + this.id + " because " + error.message);
+        throw new Error("Could not start container " + self.id + " because " + error.message);
     });
 };
 
 Container.prototype.inspect = function () {
+    var self = this;
     return Q.npost(this.container, "inspect", arguments)
     .catch(function (error) {
-        throw new Error("Could not inspect container " + this.id + " because " + error.message);
+        throw new Error("Could not inspect container " + self.id + " because " + error.message);
     });
 };
 
 Container.prototype.stop = function () {
+    var self = this;
     return Q.npost(this.container, "stop", arguments)
     .catch(function (error) {
-        throw new Error("Could not stop container " + this.id + " because " + error.message);
+        throw new Error("Could not stop container " + self.id + " because " + error.message);
     });
 };
 
 Container.prototype.remove = function () {
+    var self = this;
     return Q.npost(this.container, "remove", arguments)
     .catch(function (error) {
-        throw new Error("Could not remove container " + this.id + " because " + error.message);
+        throw new Error("Could not remove container " + self.id + " because " + error.message);
     });
 };
