615a7f0d37e1aa03357f1d01e9cd76d683b41e6a
Kris Kowal <kris.kowal@cixar.com>
2014-04-18 18:19:05 -0700

    Uglify Filament, first steps
    1.  Adds support for npm run enter-mop-mode and npm run exit-mop-mode,
    which set up the local Vagrant environment for testing mopped products.
    2.  Adds support for npm run mop, which provisionally just uglifies all
    of the JavaScript
    3.  Produces a filament.tgz which will eventually be picked up by the
    web server image generation script, and also generates the indexes that
    will need to be added to the login image.
    
    Temporarily uses a .mop directory. This will need to be folded into the
    .deploy structure.

---
+++ deploy/build/fakemop.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+var Q = require("q");
+var optimist = require("optimist");
+var uglify = require("mop/lib/minify-javascript");
+var FS = require("q-io/fs");
+var argv = optimist(process.argv).argv;
+
+var targetRoot = argv.t;
+
+return FS.removeTree(FS.join(targetRoot, "filament"))
+.catch(function () {})
+.then(function () {
+    return FS.listTree(".", function (source, stat) {
+        source = FS.normal(source);
+        var sourceBase = FS.base(source);
+        if (/^\./.test(sourceBase))
+            return null;
+        var target = FS.join(targetRoot, "filament", source);
+        var targetParent = FS.directory(target);
+        return stat.isFile();
+    }).invoke("reduce", function (ready, source) {
+        var target = FS.join(targetRoot, "filament", source);
+        var targetParent = FS.directory(target);
+        return ready.then(function () {
+            return FS.makeTree(targetParent)
+            .then(function () {
+                if (/\.js$/.test(source)) {
+                    return FS.read(source, {charset: "utf-8"})
+                    .then(uglify)
+                    .then(function (content) {
+                        console.log("uglified", target);
+                        return FS.write(target, content, {charset: "utf-8"});
+                    }, function (error) {
+                        console.log("copied non parsable", target);
+                        return FS.copy(source, target);
+                    });
+                } else {
+                    console.log("copied", target);
+                    return FS.copy(source, target);
+                }
+            });
+        });
+    }, Q());
+})
+.done();
+
