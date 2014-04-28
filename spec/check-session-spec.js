bd303532437c8a576819725645eb8dbef2ba7e55
Stuart Knightley <stuart@stuartk.com>
2014-04-28 11:07:19 -0700

    Handle missing session and errors getting user in CheckSession
    and add specs

---
+++ spec/check-session-spec.js
@@ -0,0 +1,92 @@
+var Q = require("q");
+var CheckSession = require("../check-session");
+
+describe("CheckSession", function () {
+    var checkSession, ok, notOk;
+
+    beforeEach(function () {
+        ok = jasmine.createSpy().andReturn("ok");
+        notOk = jasmine.createSpy().andReturn("not ok");
+        checkSession = CheckSession(ok, notOk);
+    });
+
+    it("defaults to redirect if no notOk function is given", function (done) {
+        checkSession = CheckSession(ok);
+        checkSession({})
+        .then(function (response) {
+            expect(response.status).toEqual(302);
+            expect(response.headers.Location).toEqual("http://local-firefly.declarativ.net:2440");
+        })
+        .done(done, done);
+    });
+
+    it("returns notOk(request) if there is no session", function (done) {
+        var request = {};
+        checkSession(request)
+        .then(function (response) {
+            expect(notOk).toHaveBeenCalledWith(request);
+            expect(response).toEqual("not ok");
+        })
+        .done(done, done);
+    });
+
+    it("returns notOk(request) if there is no githubUser on the session", function (done) {
+        var request = {session: {}};
+        checkSession(request)
+        .then(function (response) {
+            expect(notOk).toHaveBeenCalledWith(request);
+            expect(response).toEqual("not ok");
+        })
+        .done(done, done);
+    });
+
+    it("returns notOk(request) if there is no githubUser on the session", function (done) {
+        var request = {session: {}};
+        checkSession(request)
+        .then(function (response) {
+            expect(notOk).toHaveBeenCalledWith(request);
+            expect(response).toEqual("not ok");
+        })
+        .done(done, done);
+    });
+
+    it("returns notOk(request) if there is no githubUser on the session", function (done) {
+        var request = {session: {}};
+        checkSession(request)
+        .then(function (response) {
+            expect(notOk).toHaveBeenCalledWith(request);
+            expect(response).toEqual("not ok");
+        })
+        .done(done, done);
+    });
+
+    it("returns ok(request) if there is a valid githubUser", function (done) {
+        var request = {session: {githubUser: Q({})}};
+        checkSession(request)
+        .then(function (response) {
+            expect(ok).toHaveBeenCalledWith(request);
+            expect(response).toEqual("ok");
+        })
+        .done(done, done);
+    });
+
+    it("returns notOk(request) if there is not a valid githubUser", function (done) {
+        var request = {session: {githubUser: Q(void 0)}};
+        checkSession(request)
+        .then(function (response) {
+            expect(notOk).toHaveBeenCalledWith(request);
+            expect(response).toEqual("not ok");
+        })
+        .done(done, done);
+    });
+
+    it("returns notOk(request) if there is an error getting githubUser", function (done) {
+        var request = {session: {githubUser: Q.reject(new Error())}};
+        checkSession(request)
+        .then(function (response) {
+            expect(notOk).toHaveBeenCalledWith(request);
+            expect(response).toEqual("not ok");
+        })
+        .done(done, done);
+    });
+});
