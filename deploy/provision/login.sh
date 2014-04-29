726687fcb54cffefcb9c11620961eba0060c8be5
Pierre Frisch <pierre.frisch@spearway.com>
2014-04-29 10:23:04 -0700

    Add a hash tag to the URL to force cache refresh

--- deploy/provision/login.sh
+++ deploy/provision/login.sh
@@ -6,6 +6,19 @@ pushd /srv
 
 if [[ -e "/srv/filament.tgz" ]]; then
     tar -xzf filament.tgz
+    pushd filament
+        # Change the index to account for the hash key
+        if [[ -e GIT_HASH ]]; then
+    	    export GIT_HASH=`cat GIT_HASH`
+            sed -i.bak 's?<base href="/app/">?<base href="/app/'$GIT_HASH'/">?' firefly-index.html
+            sed -i.bak 's?data-package="/app"?data-package="/app/'$GIT_HASH'"?' firefly-index.html
+            sed -i.bak 's?<base href="/app/login/">?<base href="/app/'$GIT_HASH'/login/">?' login/index.html
+            sed -i.bak 's?data-package="/app"?data-package="/app/'$GIT_HASH'"?' login/index.html
+            sed -i.bak 's?/assets/videos?/app/'$GIT_HASH'/assets/videos?' login/index.html
+            sed -i.bak 's?<base href="/app/project-list/">?<base href="/app/'$GIT_HASH'/project-list/">?' project-list/index.html
+            sed -i.bak 's?data-package="/app"?data-package="/app/'$GIT_HASH'"?' project-list/index.html
+        fi
+    popd
     chown -R montage:montage filament
 fi
 if [[ -e "/srv/firefly.tgz" ]]; then
--- deploy/provision/web-server.sh
+++ deploy/provision/web-server.sh
@@ -6,8 +6,20 @@ pushd /srv
 
 if [[ -e "/srv/filament.tgz" ]]; then
     tar -xzf filament.tgz
-    mv filament app
-    chown -R montage:montage app
+    export BASE_DIR="app"
+    export TARGET_DIR="$BASE_DIR"
+    if [[ -e "filament/GIT_HASH" ]]; then
+        export GIT_HASH=`cat filament/GIT_HASH`
+        export TARGET_DIR="$BASE_DIR/$GIT_HASH"
+    fi
+    mkdir -p "$TARGET_DIR"
+    mv -f filament/* "$TARGET_DIR"
+    pushd "$TARGET_DIR"
+        find . -iname '*.css' | while read -r file ; do
+            sed -i.bak 's?url(/assets/">?url(/app/'$GIT_HASH'/assets/">?' "$file"
+        done
+    popd
+    chown -R montage:montage "$TARGET_DIR"
     rm -rf filament.tgz
     # We need to token file at the rooot to respond to the haproxy requests
     echo "<html></html>" > index.html
