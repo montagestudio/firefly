f4cb96c40d5d4f7c3f3f930761f0790c425dee6f
Stuart Knightley <stuart@stuartk.com>
2014-05-05 19:37:00 -0700

    Wrap recurly errors in a real Error object

--- recurly.js
+++ recurly.js
@@ -16,6 +16,7 @@ function Recurly(config) {
 
         Object.keys(originalRoutes).forEach(function (route) {
             var fn = originalRoutes[route];
+            var errorFn = makeErrorFunction(apiObject, route);
 
             if (route === "list") {
                 // The list functions have callback as first
@@ -24,7 +25,8 @@ function Recurly(config) {
                 routes[route] = function (filter) {
                     var deferred = Q.defer();
                     fn(deferred.makeNodeResolver(), filter);
-                    return deferred.promise;
+                    return deferred.promise
+                    .catch(errorFn);
                 };
             } else if (route === "listByAccount") {
                 // The listByAccount functions have callback as their second
@@ -33,7 +35,8 @@ function Recurly(config) {
                 routes[route] = function (accountCode, filter) {
                     var deferred = Q.defer();
                     fn(accountCode, deferred.makeNodeResolver(), filter);
-                    return deferred.promise;
+                    return deferred.promise
+                    .catch(errorFn);
                 };
             } else if (apiObject === "transactions" && route === "refund") {
                 // The transaction.refund function has a callback as the second
@@ -42,15 +45,55 @@ function Recurly(config) {
                 routes[route] = function (id, amount) {
                     var deferred = Q.defer();
                     fn(id, deferred.makeNodeResolver(), amount);
-                    return deferred.promise;
+                    return deferred.promise
+                    .catch(errorFn);
                 };
             } else {
                 // All other functions have the callback as their last argument
+                routes[route] = function () {
+                    // Borrowed from Q
+                    // https://github.com/kriskowal/q/blob/7b02ba1163085c74987b036d59c5efbe0ef987d4/q.js#L1781-L1790
+                    // so that we can catch rejections
+                    var args = Array.prototype.slice.call(arguments);
+                    var deferred = Q.defer();
+                    args.push(deferred.makeNodeResolver());
+                    Q(fn).fapply(args).catch(deferred.reject);
 
-                routes[route] = Q.denodeify(fn);
+                    return deferred.promise
+                    .catch(errorFn);
+                };
             }
         });
     });
 
     return wrapped;
 }
+
+// node-recurly errors are actually responses. This converts them
+function makeErrorFunction(apiObject, route) {
+    return function (response) {
+        var error;
+        if (response.data && response.data.error && response.data.error.description) {
+            error = new Error("Recurly " + route + " " + apiObject + " error: " + response.data.error.description._);
+        } else {
+            error = new Error("Recurly " + route + " " + apiObject + " error");
+        }
+        error.data = response.data;
+
+        throw error;
+    };
+}
+
+// { statusCode: 404,
+//   headers:
+//    { server: 'Blackhole',
+//      date: 'Fri, 02 May 2014 18:54:38 GMT',
+//      'content-type': 'application/xml; charset=utf-8',
+//      'transfer-encoding': 'chunked',
+//      connection: 'close',
+//      vary: 'Accept-Encoding',
+//      'content-language': 'en-US',
+//      'cache-control': 'no-cache',
+//      'x-request-id': 'a5nn5g42kve8o6t8mc20' },
+//   data: { error: { symbol: 'not_found', description: { _: 'Couldn\'t find Account with account_code = asdasdf',
+  // '$': { lang: 'en-US' } } } } }
