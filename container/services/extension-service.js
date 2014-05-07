803e20369fe5bde50a0cd8c76970f7e7a7c7b3ce
Jean-Romain Prévost <jr@ailleurs.me>
2014-05-06 20:54:54 -0700

    Enable loading extension from container

--- container/services/extension-service.js
+++ container/services/extension-service.js
@@ -22,12 +22,14 @@ function ExtensionService(session, fs, environment, _, __, clientPath) {
      *  - From a marketplace
      *  - From a user's own selection of available extensions
      */
-    var convertPathToExtensionUrl = function (path) {
+    var convertPathToExtensionUrl = function (path, base) {
         var projectHost = environment.getAppUrl() + "/app/extensions",
             url = null;
 
         if (new RegExp(extensionsRoot).test(path)) {
             url = projectHost + path.replace(extensionsRoot, "");
+        } else {
+            url = base + path;
         }
 
         return url;
@@ -39,6 +41,8 @@ function ExtensionService(session, fs, environment, _, __, clientPath) {
 
         if (new RegExp(projectHost).test(url)) {
             path = extensionsRoot + url.replace(projectHost, "");
+        } else {
+            path = url.replace(/https?:\/\/[^\/]+/, "");
         }
 
         return path;
@@ -46,7 +50,6 @@ function ExtensionService(session, fs, environment, _, __, clientPath) {
 
     service.getExtensions = function(extensionFolder) {
         extensionFolder = extensionFolder || extensionsRoot;
-
         return QFS.listTree(extensionFolder, function (filePath) {
             // if false return null so directories aren't traversed
             return PATH.extname(filePath).toLowerCase() === ".filament-extension" ? true : (filePath ===  extensionFolder ? false : null);
@@ -58,27 +61,33 @@ function ExtensionService(session, fs, environment, _, __, clientPath) {
     };
 
     service.listLibraryItemUrls = function (extensionUrl, packageName) {
-        var libraryItemsPath = PATH.join(convertExtensionUrlToPath(extensionUrl), "library-items", packageName);
+        var FS = (extensionUrl.indexOf(environment.getAppUrl()) != -1) ? QFS : fs,
+            path = convertExtensionUrlToPath(extensionUrl),
+            baseURL = (extensionUrl.match(/https?:\/\/[^\/]+/))[0],
+            libraryItemsPath = PATH.join(path, "library-items", packageName);
 
-        return QFS.listTree(libraryItemsPath, function (filePath) {
+        return FS.listTree(libraryItemsPath, function (filePath) {
             return PATH.extname(filePath).toLowerCase() === ".library-item" ? true : (filePath ===  libraryItemsPath ? false : null);
         }).then(function (filePaths) {
             return Q.all(filePaths.map(function (filePath) {
-                return QFS.stat(filePath).then(function (stat) {
-                    return convertPathToExtensionUrl(filePath) + (stat.isDirectory() ? "/" : "");
+                return FS.stat(filePath).then(function (stat) {
+                    return convertPathToExtensionUrl(filePath, baseURL) + (stat.isDirectory() ? "/" : "");
                 });
             }));
         });
     };
 
     service.listModuleIconUrls = function (extensionUrl, packageName) {
-        var iconsPath = PATH.join(convertExtensionUrlToPath(extensionUrl), "icons", packageName);
+        var FS = (extensionUrl.indexOf(environment.getAppUrl()) != -1) ? QFS : fs,
+            path = extensionUrl.replace(/https?:\/\/[^\/]+/, ""),
+            baseURL = (extensionUrl.match(/https?:\/\/[^\/]+/))[0],
+            iconsPath = PATH.join(path, "icons", packageName);
 
-        return QFS.listTree(iconsPath, function (filePath) {
+        return FS.listTree(iconsPath, function (filePath) {
             return PATH.extname(filePath).toLowerCase() === ".png";
         }).then(function (filePaths) {
             return filePaths.map(function (filePath) {
-                return convertPathToExtensionUrl(filePath);
+                return convertPathToExtensionUrl(filePath, baseURL);
             });
         });
     };
