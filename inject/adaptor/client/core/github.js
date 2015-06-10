ab80b69c811d647bf7a007d0dc333a5fd13e6402
Pierre Chaussalet <pchaussalet@gmail.com>
2015-06-11 01:54:00 +0200

    Avoid querying multiple times for github token

--- inject/adaptor/client/core/github.js
+++ inject/adaptor/client/core/github.js
@@ -3,6 +3,8 @@ var Promise = require("montage/core/promise").Promise;
 var GithubFs = require("./fs-github");
 var GithubApi = require("./github-api");
 
+var token;
+
 exports.githubFs = function(username, repository) {
     var deferred = Promise.defer();
 
@@ -30,21 +32,26 @@ function AuthToken() {
     var pendingTimeout;
     var timeout = 5000;
     var response = Promise.defer();
-    var request = new XMLHttpRequest();
-    request.open("GET", "/auth/github/token", true);
-    request.onreadystatechange = function () {
-        if (request.readyState === 4) {
-            if (request.status === 200) {
-                if(pendingTimeout) {
-                    clearTimeout(pendingTimeout);
+    if (!token) {
+        var request = new XMLHttpRequest();
+        request.open("GET", "/auth/github/token", true);
+        request.onreadystatechange = function () {
+            if (request.readyState === 4) {
+                if (request.status === 200) {
+                    if(pendingTimeout) {
+                        clearTimeout(pendingTimeout);
+                    }
+                    token = request.responseText;
+                    response.resolve(request.responseText);
+                } else {
+                    response.reject("HTTP " + request.status + " for /auth/token");
                 }
-                response.resolve(request.responseText);
-            } else {
-                response.reject("HTTP " + request.status + " for /auth/token");
             }
-        }
-    };
-    pendingTimeout = setTimeout(response.reject, timeout - 50);
-    request.send();
+        };
+        pendingTimeout = setTimeout(response.reject, timeout - 50);
+        request.send();
+    } else {
+        response.resolve(token);
+    }
     return response.promise.timeout(timeout);
 }
