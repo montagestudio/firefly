13ec8e9e6e9184531d95f11b9ccd5169ee204f77
Pierre Chaussalet <pchaussalet@gmail.com>
2015-09-22 02:37:35 +0200

    Add github organization retrieval

--- services/repository/github.js
+++ services/repository/github.js
@@ -1,7 +1,7 @@
 var Q = require("q");
 var GithubApi = require("github");
 
-exports.GithubService = GithubService = function(accessToken) {
+var GithubService = exports.GithubService = function(accessToken) {
     this._githubApi = new GithubApi({
         version: '3.0.0',
         headers: {
@@ -18,5 +18,11 @@ GithubService.prototype.getRepo = function(owner, repo) {
     return Q.denodeify(this._githubApi.repos.get)({
         user: owner,
         repo: repo
+    });
+};
+
+GithubService.prototype.getOrganizations = function(username) {
+    return Q.denodeify(this._githubApi.orgs.getFromUser)({
+        user: username
     });
 };
