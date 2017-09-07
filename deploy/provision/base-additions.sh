067a6d87a68f5f9775ba803c819ddd75beb7b299
Corentin Debost <codebost@gmail.com>
2017-09-06 20:06:06 -0700

    Revert "update container and base image nodejs version from 0.10.x to 4.x"

--- deploy/provision/base-additions.sh
+++ deploy/provision/base-additions.sh
@@ -8,16 +8,14 @@ apt-get install --yes git
 
 # To save time lets load the new repositories first
 # For Node
-curl -sL https://deb.nodesource.com/setup_4.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-
+add-apt-repository --yes ppa:chris-lea/node.js
 # For Redis
 add-apt-repository --yes ppa:chris-lea/redis-server
 # And be sure we are up to date
 apt-get update --fix-missing
 
 # Node
-apt-get install --yes nodejs build-essential
+apt-get install --yes nodejs
 
 # Run with naught for zero-downtime deploys
 npm install -g naught
