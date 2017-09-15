f8378f62e03244d81a0f96d4bfb3ce8ce49c9fcf
Corentin Debost <codebost@gmail.com>
2017-09-14 21:30:24 -0700

    Add logging to docker-swarm-start

--- docker-swarm-start.sh
+++ docker-swarm-start.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
+print_status()
+{
+    # $1: status
+    echo -e "\033[32m$1\033[0m"
+}
+
 # Init Swarm
-docker swarm init
+print_status "Initializing swarm"
+docker swarm init --advertise-addr 192.168.0.63
+SWARM_TOKEN=$(docker swarm join-token -q worker)
+SWARM_MASTER=$(docker info --format "{{.Swarm.NodeAddr}}")
+
+echo "Swarm master IP: $SWARM_MASTER"
 
 # Create docker swarm registry
-docker service create --name docker-registry --publish 5000:5000 registry:2
-docker ps | grep -q registry:2
+print_status "Creating swarm registry"
+docker service create --name docker-registry --publish 5000:5000 --detach=true \
+    registry:2
 
-# To visualize Docker Swarm and Services Status 
-# Install image bellow, then visit (http://localhost:5001/)
-docker run -it -d -p 5001:8080 -v /var/run/docker.sock:/var/run/docker.sock dockersamples/visualizer
+# Start visualizer to visualize Docker Swarm and Services Status
+# Visit http://localhost:5001/
+print_status "Creating swarm visualizer at localhost:5001"
+docker run -it -d --name swarm-visualizer \
+    -p 5001:8080 \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    dockersamples/visualizer
 
 # Start services to check docker-compose.yml
+print_status "Checking docker-compose.yml"
 docker-compose rm
 docker-compose up -d --build
 
@@ -20,6 +37,9 @@ docker-compose down --volumes
 docker-compose stop
 
 # Push to docker swarm registry
-#docker-compose push
+print_status "Pushing to docker swarm registry"
+# docker-compose push
+
+print_status "Deploying swarm"
 docker stack deploy --compose-file docker-compose.yml firefly
 docker stack services firefly
