fa7f77d41a179122b021a3d4078a3fbb150b7ad1
Corentin Debost <codebost@gmail.com>
2018-02-16 20:31:33 -0800

    Remove swarm initialization from scripts

--- docker-swarm-start.sh
+++ docker-swarm-start.sh
@@ -6,25 +6,6 @@ print_status()
     echo -e "\033[32m$1\033[0m"
 }
 
-# Init Swarm
-print_status "Initializing swarm"
-INIT_CODE=
-if [ -z "$IP" ]; then
-    docker swarm init
-else
-    docker swarm init --advertise-addr "$IP"
-fi
-INIT_CODE=$?
-if [ $INIT_CODE -ne 0 ]; then
-    echo "Initializing swarm failed. You may need to specify an advertise addr by setting an IP environment variable"
-    exit $INIT_CODE
-fi
-
-SWARM_TOKEN=$(docker swarm join-token -q worker)
-SWARM_MASTER=$(docker info --format "{{.Swarm.NodeAddr}}")
-
-echo "Swarm master IP: $SWARM_MASTER"
-
 # Create docker swarm registry
 print_status "Creating swarm registry"
 docker service create --name docker-registry --publish 5000:5000 --detach=true \
@@ -45,7 +26,7 @@ docker build -t firefly_project -f ./project/Dockerfile .
 
 # Push to docker swarm registry
 print_status "Pushing to docker swarm registry"
-# docker-compose push
+docker-compose push
 
 print_status "Deploying swarm"
 docker stack deploy --compose-file docker-compose.yml firefly
--- docker-swarm-stop.sh
+++ docker-swarm-stop.sh
@@ -3,4 +3,7 @@
 # Shutdown
 docker stack rm firefly
 docker service rm docker-registry
-docker swarm leave --force
+
+docker-machine ssh firefly1 "docker network prune -f"
+docker-machine ssh firefly2 "docker network prune -f"
+docker-machine ssh firefly3 "docker network prune -f"
