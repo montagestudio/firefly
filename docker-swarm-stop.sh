98d22fefa3c50f7c1de6c94f5bb8fbd077e5f26e
Corentin Debost <codebost@gmail.com>
2018-02-19 14:10:31 -0800

    Remove image building from start/stop process

--- docker-swarm-stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Shutdown
-docker stack rm firefly
-
-# Sometimes swarm networks don't get cleaned up properly, prune them
-docker-machine ssh firefly1 "docker network prune -f"
-docker-machine ssh firefly2 "docker network prune -f"
-docker-machine ssh firefly3 "docker network prune -f"
