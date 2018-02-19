1e8630ef1d3a22727a29daa014c6caafde57d508
Corentin Debost <codebost@gmail.com>
2018-02-18 18:04:40 -0800

    Add comment clarifying docker-swarm-stop

--- docker-swarm-stop.sh
+++ docker-swarm-stop.sh
@@ -4,6 +4,7 @@
 docker stack rm firefly
 docker service rm docker-registry
 
+# Sometimes swarm networks don't get cleaned up properly, prune them
 docker-machine ssh firefly1 "docker network prune -f"
 docker-machine ssh firefly2 "docker network prune -f"
 docker-machine ssh firefly3 "docker network prune -f"
