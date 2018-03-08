0ef521deffa33f33ecbf1bc5203bb1ef5b4e155e
Corentin Debost <codebost@gmail.com>
2018-03-07 16:35:17 -0800

    Fix bad ../logging path

--- project-daemon/subdomain-details-map.js
+++ project-daemon/subdomain-details-map.js
@@ -1,4 +1,4 @@
-var log = require("../logging").from(__filename);
+var log = require("../common/logging").from(__filename);
 var Set = require("collections/map");
 var routeProject = require("../common/route-project");
 var generateAccessCode = require("./generate-access-code");
