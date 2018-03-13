88778dc190e6f83fd4028ed778357c3d06360dd5
Corentin Debost <codebost@gmail.com>
2018-03-12 17:16:47 -0700

    Remove restart-all script (npm start is better)

--- restart-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for m in $(docker-machine ls -q); do
-    if echo "$m" | grep -q firefly; then
-        docker-machine ssh "$m" "docker container ls -q | xargs docker container restart"
-    fi
-done
