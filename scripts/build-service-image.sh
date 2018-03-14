e2fd1faba3c907e89ec302fdd79f7daeef739334
Corentin Debost <codebost@gmail.com>
2018-03-13 19:49:29 -0700

    Fix image build script

--- scripts/build-service-image.sh
+++ scripts/build-service-image.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # $1: Image/service name
 
-docker build -t firefly_"$1" ./services/"$1"
+docker build -t firefly_"$1" ./"$1"
 docker tag firefly_"$1" 127.0.0.1:5000/"$1"
 docker push 127.0.0.1:5000/"$1"
