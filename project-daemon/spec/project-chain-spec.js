7e94230610f2a8729098240305f53de5af51cba0
Corentin Debost <codebost@gmail.com>
2018-04-02 18:47:06 -0700

    Fix workspace delete spec

--- project-daemon/spec/project-chain-spec.js
+++ project-daemon/spec/project-chain-spec.js
@@ -1,20 +1,14 @@
 var projectChain = require("../chain");
 var Q = require("q");
-var CheckSession = require("../common/check-session");
-var Session = require("../common/session");
-var MockSession = require("../common/spec/mocks/session");
 var mockRequest = require("../common/spec/mocks/request");
-var GithubSessionStore = require("../common/github-session-store");
 var makeContainerIndex = require("../make-container-index");
+var jwt = require("../common/jwt");
 
 describe("project chain", function () {
-    var token, username, packed, sessions, containerManager, containerIndex, chain, request;
+    var username, containerManager, containerIndex, chain, request;
     beforeEach(function () {
-        token = "0000000000000000";
         username = "jasmine";
-        packed = "7975a23812090216eb5ded40c6c9031cc8c4ebc78291c128a0bcbf614a9e807aa6331aa0";
 
-        sessions = {};
         containerManager = {
             setup: function () { return Q("1234"); },
             delete: jasmine.createSpy().andReturn(Q())
@@ -35,7 +29,7 @@ describe("project chain", function () {
         it("does not return any content", function (done) {
             request({
                 method: "OPTIONS",
-                url: "http://127.0.0.1:2440/index.html"
+                url: "http://localhost:2440/index.html"
             })
             .then(function (response) {
                 expect(response.body.join("")).toEqual("");
@@ -49,14 +43,19 @@ describe("project chain", function () {
             var details = {username: username, owner: username, repo: "repo"};
             containerIndex.set(details, null);
 
-            request({
-                method: "DELETE",
-                url: "http://api.localhost:2440/workspaces",
-                headers: {
-                    cookie: "session=abc"
+            jwt.sign({
+                githubUser: {
+                    login: username
                 }
-            })
-            .then(function (response) {
+            }).then(function (token) {
+                return request({
+                    method: "DELETE",
+                    url: "http://api.localhost:2440/workspaces",
+                    headers: {
+                        "x-access-token": token
+                    }
+                });
+            }).then(function (response) {
                 expect(response.status).toEqual(200);
                 expect(response.body.join("")).toEqual('{"deleted":true}');
                 expect(containerManager.delete).toHaveBeenCalledWith(details);
