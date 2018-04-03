ac43cad36df76c62163c973f3c983c5d26d16f96
Corentin Debost <codebost@gmail.com>
2018-04-02 18:46:57 -0700

    Remove session related specs

--- project-daemon/spec/project-chain-spec.js
+++ project-daemon/spec/project-chain-spec.js
@@ -22,8 +22,6 @@ describe("project chain", function () {
         containerIndex = makeContainerIndex();
 
         chain = projectChain({
-            sessions: MockSession(sessions),
-            checkSession: CheckSession,
             containerManager: containerManager,
             containerIndex: containerIndex
         }).end();
@@ -46,70 +44,8 @@ describe("project chain", function () {
         });
     });
 
-    describe("GET session", function () {
-        beforeEach(function () {
-            var store = new GithubSessionStore();
-            store.sessions = sessions;
-
-            chain = projectChain({
-                sessions: Session("session", "x", null, store),
-                checkSession: CheckSession,
-                containerManager: {setup: function () { return Q("1234"); }},
-                containerIndex: containerIndex
-            }).end();
-        });
-
-        it("echos session cookie", function (done) {
-            request({
-                method: "GET",
-                url: "http://127.0.0.1:2440/session?id=" + packed,
-                headers: {
-                    referer: "http://local.montage.studio:2440",
-                }
-            })
-            .then(function (response) {
-                expect(response.status).toEqual(307);
-                var setCookie = response.headers["set-cookie"][1];
-                expect(setCookie).toContain(packed);
-            }).then(done, done);
-        });
-
-        it("returns 400 bad request when the sessionId is invalid", function (done) {
-            request({
-                method: "GET",
-                url: "http://127.0.0.1:2440/session?id=xxx",
-                headers: {
-                    referer: "http://local.montage.studio:2440",
-                }
-            })
-            .then(function (response) {
-                expect(response.status).toEqual(400);
-            }).then(done, done);
-        });
-
-        // FIXME: disabled until we work out how to transfer the session when
-        // there is no referrer because of a log in through Github.
-        xit("returns 403 when the referer is invalid", function (done) {
-            request({
-                method: "GET",
-                url: "http://127.0.0.1:2440/session?id=xxx",
-                headers: {
-                }
-            })
-            .then(function (response) {
-                expect(response.status).toEqual(403);
-            }).then(done, done);
-        });
-    });
-
     describe("DELETE api/workspaces", function () {
         it("calls containerManager.delete with the container's details", function (done) {
-            // fake session
-            sessions.abc = {
-                username: username,
-                githubUser: Q({})
-            };
-
             var details = {username: username, owner: username, repo: "repo"};
             containerIndex.set(details, null);
 
