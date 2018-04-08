8bfb1f70729073e5fc22b50e9a543228f386f539
Corentin Debost <codebost@gmail.com>
2018-04-08 03:59:59 -0700

    Add project image to docker-compose to build it without a separate step

--- scripts/build-service-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# $1: Image/service name
-
-docker build -t firefly_"$1" -f ./"$1"/Dockerfile .
-docker tag firefly_"$1" 127.0.0.1:5000/"$1"
-docker push 127.0.0.1:5000/"$1"
--- scripts/rm-project-services.sh
+++ scripts/rm-project-services.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROJECT_SERVICES=$(docker service ls | grep 'project: ')
+PROJECT_SERVICES=$(docker service ls | grep 'project:')
 if [ ! -z "${PROJECT_SERVICES}" ]; then
     echo "${PROJECT_SERVICES}" | awk '{print $1}' | xargs docker service rm
 fi
