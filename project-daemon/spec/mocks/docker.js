a02af9500d34760f8038f5dc7748d771380b9006
Corentin Debost <codebost@gmail.com>
2018-04-09 14:59:48 -0700

    Improve project daemon mock

--- project-daemon/spec/mocks/docker.js
+++ project-daemon/spec/mocks/docker.js
@@ -1,18 +1,61 @@
 var Q = require("q");
 var uuid = require("uuid");
 
-module.exports = MockDocker;
-function MockDocker() {}
 
-MockDocker.prototype.createService = function () {
-    return Q({
+module.exports = MockDocker;
+function MockDocker() {
+    this.services = [];
+    this.tasks = [];
+}
+
+MockDocker.prototype.getService = function (name) {
+    var self = this;
+    return {
         id: uuid.v4(),
+        inspect: function () {
+            var info = self.services.filter(function (service) {
+                return service.Spec.Name === name;
+            })[0];
+            if (info) {
+                return Q(info);
+            } else {
+                return Q.reject(new Error("Service does not exist"));
+            }
+        }
+    };
+};
+
+MockDocker.prototype.listServices = function () {
+    return Q(this.services);
+};
+
+MockDocker.prototype.createService = function (opts) {
+    var self = this;
+    var service = {
+        ID: uuid.v4(),
+        Spec: opts
+    };
+    this.services.push(service);
+    var task = {
+        Status: {
+            State: "running"
+        }
+    };
+    this.tasks.push(task);
+
+    return Q({
+        id: service.ID,
+        inspect: function () {
+            return Q(opts);
+        },
         remove: function () {
+            self.services.splice(self.services.indexOf(service), 1);
+            self.tasks.splice(self.tasks.indexOf(task), 1);
             return Q.resolve();
         }
     });
 };
 
-MockDocker.prototype.listServices = function () {
-    return Q([]);
+MockDocker.prototype.listTasks = function () {
+    return Q(this.tasks);
 };
