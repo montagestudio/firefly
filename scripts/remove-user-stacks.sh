7f57bb9fbbcd3ef15889030b77f4827b7b3023aa
Corentin Debost <codebost@gmail.com>
2018-04-11 17:25:21 -0700

    Add script for removing all user stacks

---
+++ scripts/remove-user-stacks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+user_stacks=$(docker stack ls | grep 'firefly-project_')
+if [ ! -z "${user_stacks}" ]; then
+    echo "${user_stacks}" | awk '{print $1}' | xargs docker stack rm
+fi
--- scripts/rm-project-services.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PROJECT_SERVICES=$(docker service ls | grep 'project:')
-if [ ! -z "${PROJECT_SERVICES}" ]; then
-    echo "${PROJECT_SERVICES}" | awk '{print $1}' | xargs docker service rm
-fi
