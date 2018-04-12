82edb5760405d3840348dd7a74cf7c8f93be1f76
Corentin Debost <codebost@gmail.com>
2018-04-11 17:38:34 -0700

    Fix travis

--- scripts/remove-user-stacks.sh
+++ scripts/remove-user-stacks.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 user_stacks=$(docker stack ls | grep 'firefly-project_')
 if [ ! -z "${user_stacks}" ]; then
     echo "${user_stacks}" | awk '{print $1}' | xargs docker stack rm
