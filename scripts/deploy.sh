e7c291ad89e2b0c02fc6b2b970c62806e275c187
Corentin Debost <codebost@gmail.com>
2018-04-30 00:53:53 -0700

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+SSH_OPTIONS="-o IdentitiesOnly=yes -o LogLevel=ERROR -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 STAGING_MANAGER_IP="159.89.176.34"
 
 docker-compose -f firefly-stack.yml push
@@ -9,11 +10,11 @@ docker-compose -f firefly-stack.yml push
 if [ "$1" == "-p" ]; then
     exit 1
 else
-    scp "traefik-stack.yml" "root@${STAGING_MANAGER_IP}:/opt/app/traefik-stack.yml"
-    scp "firefly-stack.yml" "root@${STAGING_MANAGER_IP}:/opt/app/firefly-stack.yml"
-    scp "env/staging.env"   "root@${STAGING_MANAGER_IP}:/opt/app/.env"
-    scp "traefik/traefik.toml"      "root@${STAGING_MANAGER_IP}:/opt/app/traefik/traefik.toml"
-    ssh "root@${STAGING_MANAGER_IP}" 'cd /opt/app &&\
+    scp ${SSH_OPTIONS} "traefik-stack.yml" "root@${STAGING_MANAGER_IP}:/opt/app/traefik-stack.yml"
+    scp ${SSH_OPTIONS} "firefly-stack.yml" "root@${STAGING_MANAGER_IP}:/opt/app/firefly-stack.yml"
+    scp ${SSH_OPTIONS} "env/staging.env"   "root@${STAGING_MANAGER_IP}:/opt/app/.env"
+    scp ${SSH_OPTIONS} "traefik/traefik.toml"      "root@${STAGING_MANAGER_IP}:/opt/app/traefik/traefik.toml"
+    ssh ${SSH_OPTIONS} "root@${STAGING_MANAGER_IP}" 'cd /opt/app &&\
                                  export $(cat ".env") &&\
                                  docker stack deploy -c traefik-stack.yml traefik &&\
                                  docker stack deploy -c firefly-stack.yml firefly'
