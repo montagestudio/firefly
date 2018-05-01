ea502b096f5ae1fa2e9d2f7e7668d7c03fa287a2
Corentin Debost <codebost@gmail.com>
2018-05-01 02:54:13 -0700

    Update travis and deploy for compose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ set -e
 SSH_OPTIONS="-o IdentitiesOnly=yes -o LogLevel=ERROR -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 STAGING_MANAGER_IP="159.89.176.34"
 
-docker-compose -f firefly-stack.yml push
+docker-compose push
 
 if [ "$1" == "-p" ]; then
     exit 1
@@ -16,6 +16,5 @@ else
     scp ${SSH_OPTIONS} "traefik/traefik.toml"      "root@${STAGING_MANAGER_IP}:/opt/app/traefik/traefik.toml"
     ssh ${SSH_OPTIONS} "root@${STAGING_MANAGER_IP}" 'cd /opt/app &&\
                                  export $(cat ".env") &&\
-                                 docker stack deploy -c traefik-stack.yml traefik &&\
-                                 docker stack deploy -c firefly-stack.yml firefly'
+                                 docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d'
 fi
