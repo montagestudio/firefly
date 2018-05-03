a153c56146f9c7afa24ae1304de1f6bdf9d9e8f7
Corentin Debost <codebost@gmail.com>
2018-05-02 17:01:31 -0700

    Improve deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,16 +5,21 @@ set -e
 SSH_OPTIONS="-o IdentitiesOnly=yes -o LogLevel=ERROR -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 STAGING_MANAGER_IP="159.89.176.34"
 
+# Push tags, staging/prod will pull these tags before starting
 docker-compose push
+
+# Create an archive of firefly to upload to environments
+tar -cp --exclude='**/node_modules' --exclude=node_modules --exclude='.git' -f firefly.tar.gz .
 
 if [ "$1" == "-p" ]; then
     exit 1
 else
-    scp ${SSH_OPTIONS} "traefik-stack.yml" "root@${STAGING_MANAGER_IP}:/opt/app/traefik-stack.yml"
-    scp ${SSH_OPTIONS} "firefly-stack.yml" "root@${STAGING_MANAGER_IP}:/opt/app/firefly-stack.yml"
-    scp ${SSH_OPTIONS} "env/staging.env"   "root@${STAGING_MANAGER_IP}:/opt/app/.env"
-    scp ${SSH_OPTIONS} "traefik/traefik.toml"      "root@${STAGING_MANAGER_IP}:/opt/app/traefik/traefik.toml"
-    ssh ${SSH_OPTIONS} "root@${STAGING_MANAGER_IP}" 'cd /opt/app &&\
-                                 export $(cat ".env") &&\
-                                 docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d'
+    scp ${SSH_OPTIONS} "firefly.tar.gz" "root@${STAGING_MANAGER_IP}:/firefly.tar.gz"
+    ssh ${SSH_OPTIONS} "root@${STAGING_MANAGER_IP}" "cd /opt/app && tar -xpf /firefly.tar.gz"
+    ssh ${SSH_OPTIONS} "root@${STAGING_MANAGER_IP}" 'cd /opt/app/ && \ 
+                                                     export $(cat env/staging.env) && \
+                                                     docker-compose pull && \
+                                                     docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d --no-build'
 fi
+
+rm firefly.tar.gz
