9d63c4c5d98548baf8f48b400fa0afab8514716d
Corentin Debost <codebost@gmail.com>
2018-05-20 19:20:35 -0700

    Implement specs and routes for /workspaces

--- api-gateway/routes.js
+++ api-gateway/routes.js
@@ -1,7 +1,73 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const apiEndpoint = require('./api-endpoint');
 
-module.exports = (app) => {
-    app.use(bodyParser());
+module.exports = (app, request, getJwtProfile) => {
+    app.use(bodyParser.json());
     app.use(cors());
+
+    app.use(async (req, res, next) => {
+        const accessToken = req.headers['x-access-token'];
+        try {
+            const info = await getJwtProfile(`Bearer ${accessToken}`);
+            Object.assign(req, info);
+            next();
+        } catch (error) {
+            res.status(401);
+            if (error.response && error.response.status === 400) {
+                res.json({ error: 'Unauthorized'});
+            } else {
+                console.error('Unexpected authorization error: ' + error);
+                res.json({ error: 'Unexpected authorization error'});
+            }
+        }
+    });
+
+    app.route('/workspaces')
+        .get(apiEndpoint(async (req, res, next) => {
+            let workspaces;
+            try {
+                workspaces = await request.get('http://project-daemon/workspaces');
+            } catch (error) {
+                workspaces = [];
+            }
+            res.json(workspaces);
+        }))
+        .delete(apiEndpoint(async (req, res, next) => {
+            let response;
+            try {
+                response = await request.delete('http://project-daemon/workspaces');
+            } catch (error) {
+                response = { deleted: false };
+            }
+            res.json(response);
+        }));
+
+    app.all('/:owner/:repo/*', apiEndpoint(async (req, res, next) => {
+        try {
+            const response = await request[req.method.toLowerCase()].call(request);
+            res.json(response);
+        } catch (error) {
+            const { response } = error;
+            if (response) {
+                res.status(response.status);
+                res.send(typeof response === 'object' ? response : { error: response });
+            } else {
+                throw error;
+            }
+        }
+    }));
+
+    app.all('/*', async(req, res, next) => {
+        res.sendStatus(404);
+    });
+
+    app.use((err, req, res, next) => {
+        console.error('Unexpected error: ' + err);
+        res.status(500);
+        res.json({
+            code: 500,
+            error: 'Unexpected error'
+        });
+    });
 };
--- api-gateway/test/api-spec.js
+++ api-gateway/test/api-spec.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const request = require("supertest");
+const supertest = require("supertest");
 const chai = require("chai");
+const { expect } = chai;
+const spies = require("chai-spies");
 const routes = require("../routes");
+const URL = require('url');
+const axios = require('axios');
 
-async function fakeRequest() {
-    return {};
-}
+chai.use(spies);
 
 async function getJwtProfile(authHeader) {
     if (authHeader) {
@@ -18,26 +20,81 @@ async function getJwtProfile(authHeader) {
 }
 
 describe('api', () => {
+    let app, sandbox;
+
     beforeEach(() => {
         app = express();
-        routes(app, fakeRequest, getJwtProfile);
+        routes(app, axios, getJwtProfile);
+    });
+
+    afterEach(() => {
+        chai.spy.restore();
     });
 
     it('responds with 401 when no authentication is provided', (done) => {
-        request(app)
+        supertest(app)
             .get('/workspaces')
-            .expect(401)
-            .end(done);
+            .expect(401, done);
     });
 
     it('responds with 404 for a non-existent route', (done) => {
-        request(app)
+        supertest(app)
             .get('/doesntexist')
             .set('x-access-token', 'abc')
-            .expect(404)
-            .end(done);
+            .expect(404, done);
     });
 
     describe('GET /workspaces', () => {
+        it('proxies project-daemon\'s workspace endpoint', (done) => {
+            chai.spy.on(axios, 'get', async (url) => []);
+            supertest(app)
+                .get('/workspaces')
+                .set('x-access-token', 'abc')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    expect(axios.get).to.have.been.called.with('http://project-daemon/workspaces');
+                    done();
+                });
+        });
+
+        it('returns an empty array if the project-daemon service cannot be reached', (done) => {
+            chai.spy.on(axios, 'get', async (url) => {
+                throw new Error('chaos');
+            });
+            supertest(app)
+                .get('/workspaces')
+                .set('x-access-token', 'abc')
+                .expect(200)
+                .expect([], done);
+        });
+    });
+
+    describe('DELETE /workspaces', () => {
+        it('proxies project-daemon\'s workspace endpoint', (done) => {
+            const fakeResponse = { foo: 'bar' };
+            chai.spy.on(axios, 'delete', async (url) => fakeResponse);
+            supertest(app)
+                .delete('/workspaces')
+                .set('x-access-token', 'abc')
+                .expect(200)
+                .expect(fakeResponse)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    expect(axios.delete).to.have.been.called.with('http://project-daemon/workspaces');
+                    done();
+                });
+        });
+
+        it('returns {deleted: false} if the operation failed', (done) => {
+            chai.spy.on(axios, 'delete', async (url) => {
+                throw new Error('chaos');
+            });
+            supertest(app)
+                .delete('/workspaces')
+                .set('x-access-token', 'abc')
+                .expect(200)
+                .expect({ deleted: false }, done);
+        });
     });
 });
