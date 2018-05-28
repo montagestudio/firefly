50b6ebdca535566a3857441ebeb22fecc2df1ecd
Corentin Debost <codebost@gmail.com>
2018-05-27 19:11:47 -0700

    Add npm service

--- npm/index.js
+++ npm/index.js
@@ -1,6 +1,24 @@
-const express = require("express");
+const express = require('express');
+const routes = require('./routes');
+const QioFS = require('q-io/fs');
+const npm = require('npm');
 
-const app = express();
+const PACKAGE_HOME = process.env.PACKAGE_HOME || './';
 
-app.listen(80);
-console.log("Listening on port 80");
+QioFS.exists(PACKAGE_HOME)
+    .then((exists) => {
+        if (!exists) {
+            return QioFS.makeDirectory(PACKAGE_HOME);
+        }
+    })
+    .then(() => {
+        const app = express();
+        const fs = QioFS.reroot(PACKAGE_HOME);
+        routes(app, fs, npm);
+        app.listen(80);
+        console.log('Listening on port 80');
+    })
+    .catch((err) => {
+        console.error('ERROR while starting up. Exiting.', err);
+        process.exit(1);
+    });
