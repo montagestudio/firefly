9c1abe2fe1b9a600636a1ca39905e75823abb820
Corentin Debost <codebost@gmail.com>
2018-05-27 17:44:47 -0700

    Fix packager-manager-error inheritance from Error

--- npm/package-manager-error.js
+++ npm/package-manager-error.js
@@ -2,8 +2,9 @@ var PackageManagerError = function PackageManagerError (message, code) {
     this.name = "PackageManagerError";
     this.message = message;
     this.code = code;
+    this.stack = (new Error()).stack;
 };
 
-PackageManagerError.prototype = new Error();
+PackageManagerError.prototype = Object.create(Error.prototype);
 
 module.exports = PackageManagerError;
