e3390ed8bf5fa9d24f4431945f0ec58d4860fd8c
Corentin Debost <codebost@gmail.com>
2018-06-05 15:31:43 -0700

    Fix workspace entrypoint

--- workspace/index.js
+++ workspace/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 
-process.env.WORKSPACE_HOME = process.env.WORKSPACE_HOME || '/workspaces';
-
 const app = express();
 
-routes(app);
+routes(app, process.env.WORKSPACE_HOME || '/workspaces');
 
 app.listen(80);
 console.log('Listening on port 80');
